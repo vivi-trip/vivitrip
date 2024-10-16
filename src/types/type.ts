93c2759e3d0738488bab25e398e10fe77ac56870
JuhyeokC <bos2ablues@gmail.com>
2024-10-16 19:03:27 +0900

    [feat/#3] ReservationStatus 타입 생성

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,1 +1,10 @@
-// export type router = "app" | "pages";
+import { RESERVATION_STATUS } from "../constants/constants";
+
+export type ReservationStatusType = keyof typeof RESERVATION_STATUS;
+export type ReservationStatusWord =
+  (typeof RESERVATION_STATUS)[ReservationStatusType];
+export interface ReservationChipProps {
+  status: ReservationStatusType;
+  count: number;
+  onclick: () => void;
+}
