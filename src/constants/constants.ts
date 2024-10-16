46a4d96f0bced320e6a89aaccf4a149e76cab466
JuhyeokC <bos2ablues@gmail.com>
2024-10-16 19:03:11 +0900

    [feat/#3] RESERVATION_STATUS 객체 생성

---
+++ src/constants/constants.ts
@@ -0,0 +1,13 @@
+export const EXAMPLE = "example";
+
+/**
+ * @desc
+ * ReservationChip 컴포넌트
+ */
+export const RESERVATION_STATUS = {
+  pending: "예약",
+  confirmed: "확정",
+  declined: "거절",
+  canceled: "취소",
+  completed: "완료",
+} as const;
