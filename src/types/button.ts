fceb077286acc2ebdda3fa771fc639c09d9f71f5
hayuri1990 <hayuri1990@gmail.com>
2024-10-18 23:17:12 +0900

    [feat/#6] Button 타입 분리

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,3 +1,13 @@
+export type ButtonColorType =
+  | "white_black"
+  | "white_green"
+  | "white_gray"
+  | "black"
+  | "green"
+  | "gray";
+
+export type ButtonTextSizeType = "s" | "m" | "l" | "xl" | "xxl" | "xxxl";
+
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   type: "button" | "submit";
   width?: string;
@@ -5,14 +15,8 @@ interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   fullWidth?: boolean;
   radius?: "4" | "6" | "8" | "9" | "15";
   gap?: "4" | "8" | "10";
-  backgroundColor?:
-    | "white_black"
-    | "white_green"
-    | "white_gray"
-    | "black"
-    | "green"
-    | "gray";
-  fontStyle?: "s" | "m" | "l" | "xl" | "xxl" | "xxxl";
+  backgroundColor?: ButtonColorType;
+  fontStyle?: ButtonTextSizeType;
   onClick?: () => void;
   disabled?: boolean;
   className?: string;
