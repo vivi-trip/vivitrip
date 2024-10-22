cbd9ceb969d2b587d478af9e43d9d24553c6a208
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:32:00 +0900

    [feat/#28] 외부 이미지 주소 허용 추가

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,15 @@ const nextConfig = {
   },
   images: {
     // unoptimized: true,
-    remotePatterns: [{ protocol: "https", hostname: "**.domain.co(m|.kr)" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "**.domain.co(m|.kr)" },
+      { protocol: "https", hostname: "**.pixabay.co(m|.kr)" },
+      { protocol: "https", hostname: "**.freepik.co(m|.kr)" },
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
   },
 };
 
