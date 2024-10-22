3118edda24f00f7a5f464224e279a3a70b51ba04
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:33:39 +0900

    [feat/#28] Avatar 컴포넌트 추가

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,33 @@
+import useUserStore from "@/src/stores/userStore";
+import { ProfileImageUrl } from "@/src/types/user";
+
+export async function getServerSideProps() {
+  const user = { name: null };
+  return {
+    props: { user }, // 페이지에 전달
+  };
+}
+
+const Avatar = () => {
+  const { user } = useUserStore();
+
+  const profileImageUrl: ProfileImageUrl = JSON.parse(
+    user?.profileImageUrl ?? "",
+  );
+
+  if (!user) return null;
+
+  return (
+    <article className="flex flex-wrap items-center justify-center gap-2">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={profileImageUrl.url}
+        alt={profileImageUrl.name}
+        className="h-8 w-8 rounded-full"
+      />
+      <p className="text-16px-medium text-basic-black">{user.nickname}</p>
+    </article>
+  );
+};
+
+export default Avatar;
