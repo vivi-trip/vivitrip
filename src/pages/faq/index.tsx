04131be916d9c0492df113de86b9a216aedd59e1
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:26:45 +0900

    [feat/#28] 페이지 추가 - 로그인 페이지 - 회원가입 페이지 - 개인정보 보호정책 페이지 - 자주 묻는 질문 페이지

---
+++ src/pages/faq/index.tsx
@@ -0,0 +1,9 @@
+const FAQ = () => {
+  return (
+    <div>
+      <p>자주 묻는 질문 페이지</p>
+    </div>
+  );
+};
+
+export default FAQ;
---
+++ src/pages/sign-in/index.tsx
@@ -0,0 +1,47 @@
+import PATH_NAMES from "@/src/constants/pathname";
+import useUserStore from "@/src/stores/userStore";
+import { User } from "@/src/types/user";
+import { useRouter } from "next/navigation";
+
+export async function getServerSideProps() {
+  const user = { name: null };
+  return {
+    props: { user }, // 페이지에 전달
+  };
+}
+
+const DUMMY_USER: User = {
+  id: 0,
+  email: "dummy@user.temp",
+  nickname: "더미 사용자",
+  profileImageUrl:
+    '{"url":"https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Albaform/user/98/1729314330709/maxresdefault.jpg","name":"profileImageUrl.name"}',
+  createdAt: "2024-10-22T13:12:34.720Z",
+  updatedAt: "2024-10-22T13:12:34.720Z",
+};
+
+const SignIn = () => {
+  const { user, setUser } = useUserStore();
+  const router = useRouter();
+
+  const handleSignIn = () => {
+    setUser(DUMMY_USER);
+    router.replace(PATH_NAMES.Root);
+  };
+
+  if (user) return router.replace(PATH_NAMES.Root);
+
+  return (
+    <div className="flex min-h-[50vh] flex-col items-center justify-center gap-8">
+      <h1 className="text-center text-4xl">로그인 페이지</h1>
+      <button
+        type="button"
+        onClick={handleSignIn}
+        className="mx-auto mt-8 block rounded border border-blue-200 bg-gray-50 px-8 py-2 text-blue-200 hover:bg-blue-50">
+        임시 유저 로그인
+      </button>
+    </div>
+  );
+};
+
+export default SignIn;
