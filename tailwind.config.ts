f6e6272d3c8bb71cb631a5871219372472c1b3ee
JuhyeokC <bos2ablues@gmail.com>
2024-10-22 19:04:30 +0900

    [fix/#32] tailwind 설정 content 속성 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/constants/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{ts,tsx,js,jsx,mdx}"],
   theme: {
     extend: {
       colors: {
