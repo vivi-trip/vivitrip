17ceb026e629b1479583b8ed8c79be111d9510d6
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:36:16 +0900

    [feat/#28] 기본 레이아웃 퍼블리싱

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
+import Footer from "@/src/containers/Footer";
+import GNB from "@/src/containers/GNB";
 import "@/src/styles/globals.css";
-
 import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GNB />
+      <main className="h-main overflow-auto bg-gray-50 px-8">
+        <div className="min-h-main mx-auto max-w-screen-xl py-4">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
 };
 
 export default App;
