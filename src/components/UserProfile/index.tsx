77bf2a1780e2d49609a59aff543dd99f945ec187
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:35:03 +0900

    [feat/#28] UserProfile 컴포넌트 추가

---
+++ src/components/UserProfile/index.tsx
@@ -0,0 +1,35 @@
+import Avatar from "@/src/components/Avatar";
+import Dropdown from "@/src/components/Dropdown/Dropdown";
+import PATH_NAMES from "@/src/constants/pathname";
+import useUserStore from "@/src/stores/userStore";
+import { useRouter } from "next/router";
+
+export async function getServerSideProps() {
+  const user = { name: null };
+  return {
+    props: { user }, // 페이지에 전달
+  };
+}
+
+const UserProfile = () => {
+  const { setUser } = useUserStore();
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    setUser(null);
+    router.replace(PATH_NAMES.Root);
+  };
+
+  return (
+    <Dropdown>
+      <Dropdown.Trigger>
+        <Avatar />
+      </Dropdown.Trigger>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleSignOut}>로그아웃</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default UserProfile;
