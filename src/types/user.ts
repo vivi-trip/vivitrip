d4e1f7c11d8e957531f0c24e17623dea7615294a
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:23:51 +0900

    [feat/#28] 유저 상태관리 store 추가

---
+++ src/types/user.ts
@@ -0,0 +1,13 @@
+export interface ProfileImageUrl {
+  url: string;
+  name: string;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
