daf02faed40d5ece9e239b5e0ace16b430e3dca3
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:27:21 +0900

    [feat/#28] 홈페이지 언어 설정 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body className="antialiased">
         <Main />
