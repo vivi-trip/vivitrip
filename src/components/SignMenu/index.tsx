e774cdcfe1fb8d7c1630c94ad3908f4f88a39bc8
JuhyeokC <bos2ablues@gmail.com>
2024-10-23 05:34:46 +0900

    [feat/#28] SignMenu 컴포넌트 추가

---
+++ src/components/SignMenu/index.tsx
@@ -0,0 +1,21 @@
+import PATH_NAMES from "@/src/constants/pathname";
+import Link from "next/link";
+
+const CLASS_NAME = {
+  text: "text-16px-medium text-basic-black text-nowrap",
+};
+
+const SignMenu = () => {
+  return (
+    <div className="flex items-center gap-6">
+      <Link href={PATH_NAMES.SignIn} className={CLASS_NAME.text}>
+        로그인
+      </Link>
+      <Link href={PATH_NAMES.SignUp} className={CLASS_NAME.text}>
+        회원가입
+      </Link>
+    </div>
+  );
+};
+
+export default SignMenu;
