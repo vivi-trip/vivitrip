c48f4bea473def280633de30090aa42bf6ad677b
강효성 <goodkingdawn@naver.com>
2024-10-23 14:20:26 +0900

    feat: useDropdown 훅 구현

---
+++ src/hooks/useDropdown.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function useDropdown() {
+  const [currentValue, setCurrentValue] = useState<string | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToggleDropdown = (e: any) => {
+    e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleOffDropdown = () => {
+    setIsOpen(false);
+  };
+
+  const handleSetValue = (value: string) => {
+    setCurrentValue(value);
+  };
+
+  return {
+    isOpen,
+    handleToggleDropdown,
+    handleOffDropdown,
+    handleSetValue,
+    currentValue,
+  };
+}
+
+export default useDropdown;
