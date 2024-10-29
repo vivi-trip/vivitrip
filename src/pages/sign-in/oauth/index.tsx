d09c6429b43f7af3f04debff45e039f5581db3cc
JuhyeokC <bos2ablues@gmail.com>
2024-10-29 19:39:23 +0900

    [feat/#38] Oauth 로그인, 회원가입 처리 페이지 생성

---
+++ src/pages/sign-in/oauth/index.tsx
@@ -0,0 +1,9 @@
+const OauthSignIn = () => {
+  return (
+    <div className="flex min-h-main w-full items-center justify-center">
+      <p>처리중 입니다...</p>
+    </div>
+  );
+};
+
+export default OauthSignIn;
