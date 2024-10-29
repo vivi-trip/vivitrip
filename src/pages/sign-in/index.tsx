6408cc00fa798d155f6595547f7239cd657446e6
JuhyeokC <bos2ablues@gmail.com>
2024-10-29 19:37:27 +0900

    [feat/#38] 로그인 페이지 퍼블리싱

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,6 +1,10 @@
+import Button from "@/src/components/Button/Button";
+import Logo from "@/src/components/Logo/Logo";
+import OauthSign from "@/src/components/OauthSign";
 import PATH_NAMES from "@/src/constants/pathname";
 import useUserStore from "@/src/stores/userStore";
 import { User } from "@/src/types/user";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export async function getServerSideProps() {
@@ -32,14 +36,58 @@ const SignIn = () => {
   if (user) return router.replace(PATH_NAMES.Root);
 
   return (
-    <div className="flex min-h-[50vh] flex-col items-center justify-center gap-32">
-      <h1 className="text-center text-4xl">로그인 페이지</h1>
-      <button
-        type="button"
-        onClick={handleSignIn}
-        className="mx-auto block rounded border border-blue-200 bg-gray-50 px-32 py-8 text-blue-200 hover:bg-blue-50">
-        임시 유저 로그인
-      </button>
+    <div className="mx-auto flex min-h-main w-full max-w-640 flex-col items-stretch justify-center py-48">
+      <Logo size="lg" />
+
+      <form className="mt-56 flex flex-col gap-32" onSubmit={handleSignIn}>
+        <div className="relative">
+          <label htmlFor="login_id" className="flex flex-col gap-8">
+            <p className="text-16px-regular text-basic-black">이메일</p>
+            <input
+              type="text"
+              id="login_id"
+              placeholder="이메일을 입력해 주세요"
+              className="text-16px-regular min-h-56 rounded-6 border border-gray-500 px-20 py-12 outline-none transition-all focus:border-green-100"
+            />
+          </label>
+        </div>
+        <div className="relative">
+          <label htmlFor="login_pw" className="flex flex-col gap-8">
+            <p className="text-16px-regular text-basic-black">비밀번호</p>
+            <input
+              type="password"
+              id="login_pw"
+              placeholder="비밀번호을 입력해 주세요"
+              className="text-16px-regular min-h-56 rounded-6 border border-gray-500 px-20 py-12 outline-none transition-all focus:border-green-100"
+            />
+          </label>
+        </div>
+        <div className="relative">
+          <Button
+            type="submit"
+            height="56"
+            fullWidth
+            radius="6"
+            backgroundColor="green"
+            fontStyle="xl"
+            className="disabled:bg-gray-500">
+            로그인 하기
+          </Button>
+        </div>
+      </form>
+
+      <p className="mt-32 text-center">
+        <span className="text-16px-regular text-gray-800">
+          회원이 아니신가요?
+        </span>
+        <Link
+          href={PATH_NAMES.SignUp}
+          className="ml-8 text-green-100 underline underline-offset-2">
+          회원가입하기
+        </Link>
+      </p>
+
+      <OauthSign action="in" />
     </div>
   );
 };
