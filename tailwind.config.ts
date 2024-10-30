1594363fa9f36cdd47059101c00b1d4a6fac47c1
JuhyeokC <bos2ablues@gmail.com>
2024-10-30 17:13:39 +0900

    [chore/#55] tailwind 색상 변수 업데이트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,42 +17,65 @@ const config: Config = {
         main: "calc(100vh - (var(--header-height)) - (var(--footer-height)))",
       },
       colors: {
-        /* Black */
-        "basic-black": "var(--basic-black)",
-        "nomad-black": "var(--nomad-black)",
+        /* brand */
+        brand: {
+          600: "var(--brand-600)",
+          500: "var(--brand-500)",
+          400: "var(--brand-400)",
+          300: "var(--brand-300)",
+          200: "var(--brand-200)",
+          100: "var(--brand-100)",
+          50: "var(--brand-50)",
+        },
+
+        /* basic */
+        basic: {
+          /* Black */
+          black: "var(--basic-black)",
+        },
 
         /* Gray */
-        "gray-800": "var(--gray-800)",
-        "gray-700": "var(--gray-700)",
-        "gray-600": "var(--gray-600)",
-        "gray-500": "var(--gray-500)",
-        "gray-400": "var(--gray-400)",
-        "gray-300": "var(--gray-300)",
-        "gray-200": "var(--gray-200)",
-        "gray-100": "var(--gray-100)",
-        "gray-50": "var(--gray-50)",
-
-        /* Green */
-        "green-100": "var(--green-100)",
-        "green-50": "var(--green-50)",
-        "light-green": "var(--light-green)",
+        gray: {
+          800: "var(--gray-800)",
+          700: "var(--gray-700)",
+          600: "var(--gray-600)",
+          500: "var(--gray-500)",
+          400: "var(--gray-400)",
+          300: "var(--gray-300)",
+          200: "var(--gray-200)",
+          100: "var(--gray-100)",
+          50: "var(--gray-50)",
+        },
 
         /* Red */
-        "red-200": "var(--red-200)",
-        "red-100": "var(--red-100)",
-        "red-50": "var(--red-50)",
+        red: {
+          200: "var(--red-200)",
+          100: "var(--red-100)",
+          50: "var(--red-50)",
+        },
 
         /* Orange */
-        "orange-100": "var(--orange-100)",
-        "orange-50": "var(--orange-50)",
+        orange: {
+          100: "var(--orange-100)",
+          50: "var(--orange-50)",
+        },
 
         /* Yellow */
-        "basic-yellow": "var(--basic-yellow)",
+        yellow: {
+          200: "var(--yellow-200)",
+        },
+
+        /* Green */
+        green: {
+          200: "var(--green-200)",
+        },
 
         /* Blue */
-        "blue-200": "var(--blue-200)",
-        "blue-100": "var(--blue-100)",
-        "blue-50": "var(--blue-50)",
+        blue: {
+          200: "var(--blue-200)",
+          100: "var(--blue-100)",
+          50: "var(--blue-50)",
+        },
       },
     },
   },
