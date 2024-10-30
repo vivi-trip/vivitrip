d50b275eb00cc07f4f39c046916ccf207c4fa869
JuhyeokC <bos2ablues@gmail.com>
2024-10-30 18:47:07 +0900

    [chore/#55] vivitrip 파비콘 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
+import Favicon from "@/src/components/Favicon";
 import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>vivitrip | for your experience</title>
+        <Favicon />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
