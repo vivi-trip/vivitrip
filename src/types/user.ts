707a7ed810c3bb1cd26581c5dd1a7b7dc093cf0e
JuhyeokC <bos2ablues@gmail.com>
2024-10-31 18:05:20 +0900

    [feat/#51] user 타입 세분화

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,13 +1,30 @@
+export interface UserId {
+  id: number;
+}
+
+export interface UserEmail {
+  email: string;
+}
+
+export interface UserNickName {
+  nickname: string;
+}
+
+export interface UserPassword {
+  password: number;
+}
+
 export interface ProfileImageUrl {
   url: string;
   name: string;
 }
 
-export interface User {
-  id: number;
-  email: string;
-  nickname: string;
+export interface User extends UserId, UserEmail, UserNickName {
   profileImageUrl: string;
   createdAt: string;
   updatedAt: string;
 }
+
+export interface SignUpProps extends UserEmail, UserNickName, UserPassword {}
+
+export interface SignInProps extends UserEmail, UserPassword {}
