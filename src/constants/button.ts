c17d15871a90b710adba62101163b1b6fbf47c16
hayuri1990 <hayuri1990@gmail.com>
2024-11-02 06:58:56 +0900

    [refactor/#71] 버튼 상태별 스타일 상수화

--- src/constants/button.ts
+++ src/constants/button.ts
@@ -2,15 +2,19 @@
  * @desc
  * Button 컴포넌트
  */
-import { ButtonColorType, ButtonTextSizeType } from "@/src/types/button";
+import {
+  ButtonColorType,
+  ButtonStatusType,
+  ButtonTextSizeType,
+} from "@/src/types/button";
 
 export const BUTTON_COLOR_PRESET: Record<ButtonColorType, string> = {
   white_black: "bg-white text-brand-500 border border-brand-500",
-  white_gray: "bg-white text-brand-400 border border-gray-200",
   white_green: "bg-white text-brand-400 border border-brand-400",
+  white_gray: "bg-white text-gray-500 border border-gray-200",
   black: "bg-brand-500 text-white border-none",
-  gray: "bg-gray-500 text-white border-none",
   green: "bg-brand-400 text-white border-none",
+  gray: "bg-gray-500 text-white border-none",
 };
 
 export const BUTTON_TEXT_SIZE_PRESET: Record<ButtonTextSizeType, string> = {
@@ -20,4 +24,42 @@ export const BUTTON_TEXT_SIZE_PRESET: Record<ButtonTextSizeType, string> = {
   xl: "text-16px-bold", // 16px 700 26px
   xxl: "text-18px-regular", // 18px 400 26px
   xxxl: "text-18px-medium", // 18px 500 26px
+};
+
+export const BUTTON_STATUS_PRESET: Record<
+  ButtonStatusType,
+  string | ((backgroundColor: ButtonColorType | undefined) => string)
+> = {
+  disabled: (backgroundColor: ButtonColorType | undefined) => {
+    return backgroundColor
+      ? `${BUTTON_COLOR_PRESET[backgroundColor]} cursor-not-allowed`
+      : "bg-gray-500 text-white cursor-not-allowed";
+  },
+  hover: (backgroundColor: ButtonColorType | undefined) => {
+    const hoverClassMap: Record<ButtonColorType, string> = {
+      white_black: "hover:bg-brand-200 transition",
+      white_green: "hover:bg-brand-200 transition",
+      white_gray: "",
+      black: "hover:bg-brand-600 transition",
+      green: "hover:bg-brand-500 transition",
+      gray: "",
+    };
+    return backgroundColor ? hoverClassMap[backgroundColor] : "";
+  },
+  focus: (backgroundColor: ButtonColorType | undefined) => {
+    const focusClassMap: Record<ButtonColorType, string> = {
+      white_black:
+        "focus:outline-none focus:border-none focus:ring-2 focus:ring-brand-500",
+      white_green:
+        "focus:outline-none focus:border-none focus:ring-2 focus:ring-brand-500",
+      white_gray: "",
+      black:
+        "focus:outline-none focus:border-none focus:ring-2 focus:ring-basic-navy",
+      green:
+        "focus:outline-none focus:border-none focus:ring-2 focus:ring-basic-navy",
+      gray: "",
+    };
+    return backgroundColor ? focusClassMap[backgroundColor] : "";
+  },
+  active: "active:scale-95 active:opacity-80",
 };
