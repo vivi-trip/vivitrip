06cb94f685a1b7c50637a9c7624f3540870a25ac
JuhyeokC <bos2ablues@gmail.com>
2024-11-02 21:37:55 +0900

    [feat/#52] 기본 아바타 적용

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,24 +1,33 @@
+import IconUser from "@/assets/svgs/ic_user.svg";
 import useUserStore from "@/src/stores/userStore";
 import { ProfileImageUrl } from "@/src/types/user";
 
 const Avatar = () => {
   const { userData } = useUserStore();
 
-  const profileImageUrl: ProfileImageUrl = JSON.parse(
-    user?.profileImageUrl ?? "",
-  );
+  const profileImageUrl: ProfileImageUrl | null = (() => {
+    try {
+      return JSON.parse(userData?.profileImageUrl || "");
+    } catch {
+      return null;
+    }
+  })();
 
   if (!userData) return null;
 
   return (
     <article className="flex flex-wrap items-center justify-center gap-8">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={profileImageUrl.url}
-        alt={profileImageUrl.name}
-        className="size-32 rounded-full"
-      />
-      <p className="text-16px-medium text-basic-black">{userData.nickname}</p>
+      {profileImageUrl ? (
+        /* eslint-disable-next-line @next/next/no-img-element */
+        <img
+          src={profileImageUrl.url}
+          alt={profileImageUrl.name}
+          className="size-32 rounded-full"
+        />
+      ) : (
+        <IconUser className="text-brand-600" />
+      )}
+      <p className="text-16px-medium text-brand-600">{userData.nickname}</p>
     </article>
   );
 };
