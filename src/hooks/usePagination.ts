272d3d3c4c403224a3cf692f64db2c0fb6d88da0
hayuri1990 <hayuri1990@gmail.com>
2024-11-11 02:04:01 +0900

    [feat/#27] 페이지네이션 로직 훅으로 분리

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,51 @@
+import { PaginationProps } from "@/src/types/pagination";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const usePagination = ({
+  totalItems,
+  itemCountPerPage,
+  pageCount,
+  currentPage,
+}: PaginationProps) => {
+  const router = useRouter();
+  const totalPages = Math.ceil(totalItems / itemCountPerPage);
+  const [start, setStart] = useState(1);
+  const noPrev = start === 1;
+  const noNext = start + pageCount - 1 >= totalPages;
+
+  // 현재 페이지에 맞춰 시작 페이지(start) 계산
+  useEffect(() => {
+    const newStart = Math.floor((currentPage - 1) / pageCount) * pageCount + 1;
+    if (start !== newStart) setStart(newStart);
+  }, [currentPage, pageCount, start]);
+
+  // 화면 크기 변경 시, 페이지가 유효한 범위 내에 있도록 자동 조정
+  useEffect(() => {
+    const handleResize = () => {
+      if (currentPage > totalPages) {
+        router.push(`?page=${totalPages}`);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [currentPage, totalPages, router]);
+
+  // 페이지 이동 및 강제 새로고침
+  const navigateToPage = (page: number) => {
+    router.push(`?page=${page}`).then(() => {
+      window.location.reload();
+    });
+  };
+
+  return {
+    totalPages,
+    start,
+    noPrev,
+    noNext,
+    navigateToPage,
+  };
+};
+
+export default usePagination;
