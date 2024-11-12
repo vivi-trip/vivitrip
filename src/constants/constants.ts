76452569a58890a0fd03d8652dd0eb4f7a9c059a
JuhyeokC <bos2ablues@gmail.com>
2024-11-12 16:23:50 +0900

    [feat/#53] Swagger 에 등록한 oauth app key

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,12 @@
-// export const EXAMPLE = "example";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/**
+ * swagger oauth apps kakao provider response
+ */
+const OAUTH_APPS_RESPONSE = {
+  id: 149,
+  provider: "kakao",
+  teamId: "7-6",
+  appKey: "a0e1cf496bf462f953c891ad82041bd6",
+  createdAt: "2024-11-06T07:10:30.899Z",
+  updatedAt: "2024-11-06T07:10:30.899Z",
+};
