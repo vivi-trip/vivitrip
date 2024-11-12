90878c96a757487f9bebee114af1e77ca11bc2ea
JuhyeokC <bos2ablues@gmail.com>
2024-11-12 16:26:13 +0900

    [feat/#53] oauth 타입 생성

--- src/types/oauth.ts
+++ src/types/oauth.ts
@@ -1,7 +1,20 @@
+import { UserNickName } from "@/src/types/user";
+
 export interface OauthSignProps {
   action: "in" | "up";
 }
 
-export interface OauthSignButtonProps extends OauthSignProps {
-  provider: "kakao" | "google";
+export type OauthTypes = "google" | "kakao";
+
+export interface OauthTypeProps {
+  provider: OauthTypes;
 }
+
+export interface OauthSignButtonProps extends OauthSignProps, OauthTypeProps {}
+
+export interface OauthSignInProps extends OauthTypeProps {
+  redirectUri: string;
+  token: string;
+}
+
+export interface OauthSignUpProps extends UserNickName, OauthSignInProps {}
