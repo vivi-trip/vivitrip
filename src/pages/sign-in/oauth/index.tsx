ca39cd7c840333af7740cb1760516056dce3b984
JuhyeokC <bos2ablues@gmail.com>
2024-11-12 16:25:48 +0900

    [feat/#53] oauth 리다이렉트 라우트 경로 수정

--- src/pages/sign-in/oauth/index.tsx
+++
@@ -1,9 +0,0 @@
-const OauthSignIn = () => {
-  return (
-    <div className="flex min-h-main w-full items-center justify-center">
-      <p>처리중 입니다...</p>
-    </div>
-  );
-};
-
-export default OauthSignIn;
