e938a23bc61a752ccfae2e4a6666d357e3378c16
강효성 <goodkingdawn@naver.com>
2024-12-21 03:48:10 +0900

    [fix/#103] 팝업 모달 타이틀 동적으로 사용 가능하게 수정

--- src/components/modal/ModalPopup.tsx
+++ src/components/modal/ModalPopup.tsx
@@ -2,42 +2,21 @@ import Button from "../Button/Button";
 import useModalStore from "@/src/stores/ModalStore";
 import React from "react";
 
-// size prop의 타입을 정의합니다.
-type ModalSize = "large" | "small";
-
-// props의 타입을 정의합니다.
 interface ModalPopupProps {
-  size?: ModalSize;
+  title: string;
 }
 
-// 사이즈에 따른 스타일 클래스를 정의합니다.
-const sizeClasses: Record<ModalSize, string> = {
-  large: "h-250 w-540",
-  small: "h-220 w-327 gap-43",
-};
-
-const contentClasses: Record<ModalSize, string> = {
-  large: "font-20px-medium ",
-  small: "font-16px-medium ",
-};
-
-const buttonClasses: Record<ModalSize, string> = {
-  large: "absolute mt-40 mr-28 font-14px-medium",
-  small: "absolute left-1/2 -translate-x-1/2 w-138 h-42 font-14px-medium ",
-};
-
-const ModalPopup = ({ size = "large" }: ModalPopupProps) => {
+const ModalPopup = ({ title }: ModalPopupProps): JSX.Element => {
   const { setModalClose } = useModalStore();
 
   return (
-    <div
-      className={`relative flex flex-col justify-center rounded-12 ${sizeClasses[size]}`}>
-      <div className={`text-center text-gray-800 ${contentClasses[size]}`}>
-        가입이 완료되었습니다!
+    <div className="relative flex h-220 w-327 flex-col justify-center gap-43 rounded-12 md:h-250 md:w-540">
+      <div className="sm:font-16px-medium md:font-20px-medium text-center text-gray-800">
+        {title}
       </div>
-      <div className={size === "large" ? "flex justify-end" : ""}>
+      <div className="relative">
         <Button
-          className={`${buttonClasses[size]}`}
+          className="font-14px-medium md:font-14px-medium absolute left-1/2 h-42 w-138 -translate-x-1/2 md:left-auto md:right-28 md:h-48 md:w-120 md:translate-x-0"
           type="button"
           width="120"
           height="48"
--- src/pages/modal/index.tsx
+++ src/pages/modal/index.tsx
@@ -5,7 +5,7 @@ const OpenModalButton = () => {
   const { setModalOpen } = useModalStore();
 
   return (
-    <button type="button" onClick={() => setModalOpen(<ModalPopup />)}>
+    <button type="button" onClick={() => setModalOpen(<ModalPopup title="가입이완료 됬지롱!"/>)}>
       모달열기
     </button>
   );
