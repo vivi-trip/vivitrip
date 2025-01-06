d7d6ce704c89d63e577c6e532cee696a17d4ad85
강효성 <goodkingdawn@naver.com>
2025-01-06 11:22:37 +0900

    [feat/#103]체험 관련 타입 생성

--- src/types/activities.ts
+++ src/types/activities.ts
@@ -77,3 +77,129 @@ export interface CreateActivityReservationProps {
 export interface CreateActivityImageUrlProps {
   image: File;
 }
+export interface CursorId {
+  cursorId: number;
+}
+
+export interface Size {
+  size: number;
+}
+
+export interface Activity {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface MyActivities {
+  cursorId: CursorId[];
+  totalCount: number;
+  activities: Activity[];
+}
+
+export interface GetMyActivities extends CursorId, Size {}
+
+/** 체험등록  responses */
+export interface Experience {
+  title: string;
+  category: string;
+  description: string;
+  address: string;
+  price: number;
+  schedules: Schedule[];
+}
+
+export interface Schedule {
+  id?: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+}
+
+/** 체험 상세 조회 */
+export interface ActivityDetail {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  subImages: SubImage[];
+  schedules: Schedule[];
+}
+
+interface SubImage {
+  id: number;
+  imageUrl: string;
+}
+
+/** 체험 예약 가능일 조회 */
+export interface AvailableDate {
+  date: string;
+  times: AvailableTime[];
+}
+
+interface AvailableTime {
+  endTime: string;
+  startTime: string;
+  id: number;
+}
+
+/** 체험 리뷰조회  */
+export interface ReviewResponse {
+  averageRating: number;
+  totalCount: number;
+  reviews: Review[];
+}
+
+interface Review {
+  id: number;
+  user: User;
+  activityId: number;
+  rating: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface User {
+  profileImageUrl: string;
+  nickname: string;
+  id: number;
+}
+
+/** 체험 예약 신청 responses */
+export interface ActivityReservationResponse {
+  id: number;
+  teamId: string;
+  userId: number;
+  activityId: number;
+  scheduleId: number;
+  status: ReservationStatus;
+  reviewSubmitted: boolean;
+  totalPrice: number;
+  headCount: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+type ReservationStatus = "pending" | "confirmed" | "cancelled" | "completed";
+
+/** 체험 이미지  url 생성 */
+export interface ActivityImage {
+  activityImageUrl: string;
+}
