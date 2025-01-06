77723629951bd2c7e88fef25f858d46f9d17543e
강효성 <goodkingdawn@naver.com>
2025-01-06 11:18:00 +0900

    [feat/#103] 리뷰 모달 구현

---
+++ src/components/modal/ReviewModal/common/ReviewContent.tsx
@@ -0,0 +1,59 @@
+import clsx from "clsx";
+import Image from "next/image";
+import React from "react";
+
+interface ReviewContentProps {
+  title: string;
+  bannerImageUrl: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  headCount: number;
+  totalPrice: number;
+}
+
+const ReviewContent = ({
+  title,
+  bannerImageUrl,
+  date,
+  startTime,
+  endTime,
+  headCount,
+  totalPrice,
+}: ReviewContentProps) => {
+  return (
+    <section className={clsx("flex gap-24")}>
+      <div className={clsx("size-126", "relative")}>
+        <Image
+          src={bannerImageUrl}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-12"
+        />
+      </div>
+      <div>
+        <div className="flex h-full max-w-271 flex-col">
+          <h2 className="font-20px-bold w-full overflow-hidden truncate whitespace-nowrap">
+            {title}
+          </h2>
+          <div className="font-18px-regular mt-12 flex justify-between">
+            <span>{date}</span>
+            <span>·</span>
+            <span>
+              {startTime} - {endTime}
+            </span>
+            <span>·</span>
+            <span>{headCount} 명</span>
+          </div>
+          <span className="font-32px-bold mt-auto">
+            {" "}
+            ₩{Intl.NumberFormat().format(totalPrice)}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ReviewContent;
---
+++ src/components/modal/ReviewModal/common/ReviewForm.tsx
@@ -0,0 +1,90 @@
+import Button from "../../../Button/Button";
+import StarOff from "@/assets/svgs/ic_star_off.svg";
+import StarOn from "@/assets/svgs/ic_star_on.svg";
+import clsx from "clsx";
+import React, { useState } from "react";
+
+const ReviewForm = () => {
+  const [rating, setRating] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  // TODO: 토스트 상태 추가
+  // const [showToast, setShowToast] = useState(false);
+
+  const handleReviewChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setReviewText(event.target.value);
+  };
+
+  const handleStars = (clicked: number) => {
+    setRating(clicked);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // TODO: 성공 시 토스트 메시지 표시
+    // TODO: API 추가
+    // TODO: submit 성공시 rating, reviewText   초기화
+  };
+
+  return (
+    <>
+      <div className="flex h-100 w-432 justify-center gap-8 px-60 py-22">
+        {[1, 2, 3, 4, 5].map((id) => (
+          <div
+            key={id}
+            onClick={() => handleStars(id)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
+                handleStars(id);
+              }
+            }}
+            tabIndex={0}
+            role="button"
+            className="cursor-pointer">
+            {id <= rating ? (
+              <StarOn width={56} height={56} />
+            ) : (
+              <StarOff width={56} height={56} />
+            )}
+          </div>
+        ))}
+      </div>
+      <form
+        onSubmit={rating && reviewText ? handleSubmit : undefined}
+        className="flex w-full flex-col gap-24">
+        <textarea
+          placeholder="후기를 작성해주세요"
+          className={clsx(
+            "rounded-4 border border-gray-500 text-basic-black",
+            "font-16px-regular",
+            "px-16 py-8",
+            "h-240 w-full",
+            "resize-none",
+            "focus:border-brand-500 focus:outline-none",
+          )}
+          value={reviewText}
+          onChange={handleReviewChange}
+        />
+        <Button
+          type="button"
+          width="auto"
+          height="56"
+          radius="4"
+          gap="4"
+          fontStyle="l"
+          disabled={!rating || !reviewText}
+          className={clsx(
+            "lg:w-full",
+            !rating || !reviewText
+              ? "cursor-not-allowed bg-gray-500 text-gray-600"
+              : "border-none bg-brand-500 text-white",
+          )}>
+          작성하기
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default ReviewForm;
