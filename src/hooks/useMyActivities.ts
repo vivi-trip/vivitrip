711793a6db1bc2c9f0e51ba061095b0cee7c5b7b
강효성 <goodkingdawn@naver.com>
2025-01-06 11:21:58 +0900

    [feat/#103]내 체험 관련 훅 생성

---
+++ src/hooks/useMyActivities.ts
@@ -0,0 +1,88 @@
+import {
+  deleteArticle,
+  getMyActivities,
+  getMyReservationDashboard,
+  getMyReservedSchedule,
+  getMyResrvations,
+  patchMyActivity,
+  patchReservaionState,
+} from "../services/myActiviesApi";
+import { GetMyActivities, MyActivities } from "../types/activities";
+import {
+  PatchMyActivityParams,
+  ReservaitionState,
+  ReservatdeScheduleParams,
+  ReservationDashboardParams,
+  ReservationInfosType,
+  ReservationMonthInfosType,
+  ReservationScheduleType,
+  ReservationsParams,
+} from "../types/activitiesReservationType";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useGetMyActivities = (params: GetMyActivities) => {
+  const { data, isLoading, error } = useQuery<MyActivities>({
+    queryKey: ["myActivities", params],
+    queryFn: () => getMyActivities(params),
+  });
+
+  return { data, isLoading, error };
+};
+
+export const useGetMyReservationDashboard = (
+  params: ReservationDashboardParams,
+) => {
+  const { data, isLoading, error } = useQuery<ReservationMonthInfosType>({
+    queryKey: ["myReservationDashboard", params],
+    queryFn: () => getMyReservationDashboard(params),
+  });
+
+  return { data, isLoading, error };
+};
+
+export const useGetMyReservedSchedule = (
+  params: ReservatdeScheduleParams,
+  enabled: boolean,
+) => {
+  const { data, isLoading, error } = useQuery<ReservationScheduleType>({
+    queryKey: ["myReservedSchedule", params],
+    queryFn: () => getMyReservedSchedule(params),
+    enabled,
+  });
+  return { data, isLoading, error };
+};
+
+export const useGetMyReservations = (
+  params: ReservationsParams,
+  enabled: boolean,
+) => {
+  const { data, isLoading, error } = useQuery<ReservationInfosType>({
+    queryKey: ["myResrvations", params],
+    queryFn: () => getMyResrvations(params),
+    enabled,
+  });
+  return { data, isLoading, error };
+};
+
+export const usePatchReservationState = () => {
+  const { mutate } = useMutation({
+    mutationFn: (params: ReservaitionState) => patchReservaionState(params),
+  });
+
+  return { mutate };
+};
+
+export const useDeleteArticle = () => {
+  const { mutate } = useMutation({
+    mutationFn: (params: number) => deleteArticle(params),
+  });
+  return { mutate };
+};
+
+export const usePatchMyActivity = () => {
+  const { mutate } = useMutation({
+    mutationFn: ({ activityId, updateData }: PatchMyActivityParams) =>
+      patchMyActivity(activityId, updateData),
+  });
+  return { mutate };
+};
