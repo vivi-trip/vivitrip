5575beea47ed94923ffe3e0d7b3d3d38491488fe
강효성 <goodkingdawn@naver.com>
2025-01-06 11:14:39 +0900

    [feat/#103] 캘린더 zustand 생성

---
+++ src/stores/CalendarStore.ts
@@ -0,0 +1,35 @@
+import { ActivityDetail, Schedule } from "../types/activities";
+import formatDateToYYYYMMDD from "../utils/calendarFormatDate";
+import { create } from "zustand";
+
+interface CalendarState {
+  selectDate: Date;
+  selectSchedule: Schedule | null;
+  members: number;
+  selectMonth: number | null;
+  data: ActivityDetail;
+  onChangeSelectDate: (date: Date) => void;
+  onChangeSchedule: (schedule: Schedule | null) => void;
+  onChangeMembers: (members: number) => void;
+  onChangeSelectMonth: (month: number | null) => void;
+}
+
+export const useCalendarStore = create<CalendarState>((set) => ({
+  selectDate: new Date(),
+  selectSchedule: null,
+  members: 1,
+  selectMonth: new Date().getMonth(),
+  data: {} as ActivityDetail,
+  onChangeSelectDate: (date) => set({ selectDate: date, selectSchedule: null }),
+  onChangeSchedule: (schedule) => set({ selectSchedule: schedule }),
+  onChangeMembers: (members) => set({ members }),
+  onChangeSelectMonth: (month) => set({ selectMonth: month }),
+}));
+
+export const useCalendar = () => {
+  const store = useCalendarStore();
+  return {
+    ...store,
+    formatDate: formatDateToYYYYMMDD(store.selectDate),
+  };
+};
