a601b48a89d11dcd0d25fa800080dc3f9ea3d1b4
hayuri1990 <hayuri1990@gmail.com>
2025-01-07 22:59:39 +0900

    [feat/#24] 카테고리 필터 데이터 정의

---
+++ src/constants/filterButton.ts
@@ -0,0 +1,13 @@
+import { Category } from "@/src/types/filterButton";
+
+const CATEGORY: Category[] = [
+  { label: "모든 체험", value: "" },
+  { label: "문화 · 예술", value: "문화 · 예술" },
+  { label: "식음료", value: "식음료" },
+  { label: "스포츠", value: "스포츠" },
+  { label: "투어", value: "투어" },
+  { label: "관광", value: "관광" },
+  { label: "웰빙", value: "웰빙" },
+];
+
+export default CATEGORY;
