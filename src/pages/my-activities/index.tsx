41da83b5fa28ea97c5f11b1a543aa0fc21825dff
강효성 <goodkingdawn@naver.com>
2025-01-08 00:16:10 +0900

    [feat/#115]my-activies 페이지 생성

---
+++ src/pages/my-activities/index.tsx
@@ -0,0 +1,12 @@
+import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
+
+
+const MyActivities = () => {
+  return (
+    <div>
+      <SideNavigationMenu />
+    </div>
+  );
+};
+
+export default MyActivities;
