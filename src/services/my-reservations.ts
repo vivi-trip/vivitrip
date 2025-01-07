4b3fcbb5d9573bad577a1d44b0148bef011fcc6d
JuhyeokC <bos2ablues@gmail.com>
2025-01-07 17:03:34 +0900

    [feat/#118] MyReservations API 호출 함수 생성

---
+++ src/services/my-reservations.ts
@@ -0,0 +1,65 @@
+/**
+ * @description API 호출 함수 - MyReservations
+ */
+import {
+  CreateMyReservationReviewProps,
+  ListMyReservationsProps,
+  UpdateMyReservationProps,
+} from "../types/my-reservations";
+import api from "@/src/services/axios";
+
+/**
+ * @description 내 예약 리스트 조회
+ * @param size - 예약 갯수
+ * @param status - 예약 상태
+ * @param cursorId - 예약 커서 아이디
+ */
+export const listMyReservations = async ({
+  size,
+  status,
+  cursorId,
+}: ListMyReservationsProps) => {
+  const context = {
+    query: cursorId || size ? "?" : "",
+    size: cursorId ? `cursorId=${cursorId}` : "",
+    status: status ? `status=${status}` : "",
+    cursorId: size ? `size=${size}` : "",
+  };
+  const response = await api.get(
+    `/my-reservations${context.query}${context.size}${context.status}${context.cursorId}`,
+  );
+  return response;
+};
+
+/**
+ * @description 내 예약 수정(취소)
+ * @param reservationId - 예약 아이디
+ * @param status - 예약 상태
+ */
+export const UpdateMyReservation = async ({
+  reservationId,
+  status,
+}: UpdateMyReservationProps) => {
+  const response = await api.patch(`/my-reservations/${reservationId}`, {
+    status,
+  });
+  return response;
+};
+
+/**
+ * @description 내 예약 리뷰 작성
+ * @param reservationId - 예약 아이디
+ * @param rating - 예약 리뷰 별점
+ * @param content - 예약 리뷰 내용
+ */
+export const createMyReservationReview = async ({
+  reservationId,
+  rating,
+  content,
+}: CreateMyReservationReviewProps) => {
+  const response = await api.post(`/my-reservations/${reservationId}`, {
+    rating,
+    content,
+  });
+  return response;
+};
