162a304877054ef6665d8e3f2f8b9d4eb4b9b1a4
강효성 <goodkingdawn@naver.com>
2025-01-08 00:15:54 +0900

    [feat/#115]my-profile 페이지 생성

---
+++ src/pages/my-profile/index.tsx
@@ -0,0 +1,14 @@
+import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
+
+const MyProfile = () => {
+  const onClick = () => {
+    
+  };
+  return (
+    <div>
+      <SideNavigationMenu url={onClick} />
+    </div>
+  );
+};
+
+export default MyProfile;
