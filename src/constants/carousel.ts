d52edf78b600cd3871b344ce31c7ada5586f85a2
hayuri1990 <hayuri1990@gmail.com>
2025-01-07 22:57:38 +0900

    [feat/#24] 캐러셀 아이템 데이터 정의

---
+++ src/constants/carousel.ts
@@ -0,0 +1,61 @@
+import mainImage1 from "@/assets/pngs/mainImage1.png";
+import mainImage2 from "@/assets/pngs/mainImage2.png";
+import mainImage3 from "@/assets/pngs/mainImage3.png";
+import mainImage4 from "@/assets/pngs/mainImage4.png";
+import mainImage5 from "@/assets/pngs/mainImage5.png";
+import mainImage6 from "@/assets/pngs/mainImage6.png";
+import mainImage7 from "@/assets/pngs/mainImage7.png";
+import mainImage8 from "@/assets/pngs/mainImage8.png";
+import mainImage9 from "@/assets/pngs/mainImage9.png";
+import mainImage10 from "@/assets/pngs/mainImage10.png";
+import mainImage11 from "@/assets/pngs/mainImage11.png";
+import { CarouselItem } from "@/src/types/carousel";
+
+const CAROUSEL_ITEM: CarouselItem[] = [
+  {
+    src: mainImage1,
+    title: ["수중 사진 촬영", "푸른동굴 스노클링"],
+  },
+  {
+    src: mainImage2,
+    title: ["국내 최고 높이 보장", "패러글라이딩 체험"],
+  },
+  {
+    src: mainImage3,
+    title: ["두 발로 느끼는", "대자연 하이킹 투어"],
+  },
+  {
+    src: mainImage4,
+    title: ["포토그래퍼 동반", "인생 별빛 투어"],
+  },
+  {
+    src: mainImage5,
+    title: ["계곡에서 즐기는", "급류 래프팅 체험"],
+  },
+  {
+    src: mainImage6,
+    title: ["한복 입고 즐기는", "고즈넉한 고궁 산책"],
+  },
+  {
+    src: mainImage7,
+    title: ["베트남 전통요리", "하노이 쿠킹 클래스"],
+  },
+  {
+    src: mainImage8,
+    title: ["치유의 숲에서", "명상 원데이클래스"],
+  },
+  {
+    src: mainImage9,
+    title: ["기차 타고 떠나는", "1박2일 전주 여행"],
+  },
+  {
+    src: mainImage10,
+    title: ["여행 스트레스 싹!", "방콕 럭셔리 스파 체험"],
+  },
+  {
+    src: mainImage11,
+    title: ["듄 배싱 & 낙타 체험", "사막 사파리 투어"],
+  },
+];
+
+export default CAROUSEL_ITEM;
