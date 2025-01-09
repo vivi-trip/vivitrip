7f0078b0a2a6916d48ccfb8ec64ec8c4b4a0db09
JuhyeokC <bos2ablues@gmail.com>
2025-01-09 19:29:39 +0900

    [feat/#53] Oauth 회원 인디게이터 추가

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,10 +3,17 @@ import useUserStore from "@/src/stores/userStore";
 import { ProfileImageUrl } from "@/src/types/user";
 
 const Avatar = () => {
-  const { userData } = useUserStore();
+  const { userData, userProvider } = useUserStore();
 
   const profileImageUrl: ProfileImageUrl | null = (() => {
     try {
+      if (userData) {
+        if (typeof userData.profileImageUrl === "string")
+          return {
+            url: userData.profileImageUrl,
+            name: `${userData.nickname} 프로필 이미지`,
+          };
+      }
       return JSON.parse(userData?.profileImageUrl || "");
     } catch {
       return null;
@@ -18,12 +25,23 @@ const Avatar = () => {
   return (
     <article className="flex flex-wrap items-center justify-center gap-8">
       {profileImageUrl ? (
-        /* eslint-disable-next-line @next/next/no-img-element */
-        <img
-          src={profileImageUrl.url}
-          alt={profileImageUrl.name}
-          className="size-32 rounded-full"
-        />
+        <div className="relative">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={profileImageUrl.url}
+            alt={profileImageUrl.name}
+            className="relative size-32 rounded-full"
+          />
+          {userProvider && userProvider === "kakao" ? (
+            <div className="absolute -inset-4 rounded-full border-2 border-[#fae100] text-[0]">
+              카카오 계정
+            </div>
+          ) : (
+            <div className="absolute -inset-4 rounded-full border-2 border-b-[#34A853] border-l-[#FBBC04] border-r-[#4285F4] border-t-[#EA4335] text-[0]">
+              구글 계정
+            </div>
+          )}
+        </div>
       ) : (
         <IconUser className="text-brand-600" />
       )}
