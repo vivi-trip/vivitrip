f4bb7cc653cd07fb448bcd697e425d8b1e8102c4
JuhyeokC <bos2ablues@gmail.com>
2025-01-09 19:27:01 +0900

    [feat/#53] 경로이름 추가

--- src/constants/pathname.ts
+++ src/constants/pathname.ts
@@ -10,6 +10,8 @@ const PATH_NAMES: Record<PathNameType | TestPagePathNameType, string> = {
   FAQ: "/faq",
   Chip: "/chip",
   Button: "/button",
+  KakaoSignIn: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}/sign-in`,
+  KakaoSignUp: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}/sign-up`,
 };
 
 export default PATH_NAMES;
