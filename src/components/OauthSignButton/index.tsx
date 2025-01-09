8ae5d904a66b4a2b93cb51d99a30ae6d7a84a07a
JuhyeokC <bos2ablues@gmail.com>
2025-01-09 19:27:43 +0900

    [feat/#53] 타입, 경로 관리 수정

--- src/components/OauthSignButton/index.tsx
+++ src/components/OauthSignButton/index.tsx
@@ -1,17 +1,28 @@
 import IconGoogle from "@/assets/svgs/ic_sns_google.svg";
 import IconKakao from "@/assets/svgs/ic_sns_kakao.svg";
-import { OauthSignButtonProps } from "@/src/types/oauth";
+import PATH_NAMES from "@/src/constants/pathname";
+import type {
+  OauthActions,
+  OauthSignButtonProps,
+  OauthTypes,
+} from "@/src/types/oauth";
 import Link from "next/link";
+import { ReactNode } from "react";
 
-const OAUTH_ICONS = {
+const OAUTH_ICONS: Record<OauthTypes, ReactNode> = {
   google: <IconGoogle alt="GOOGLE Login" />,
   kakao: <IconKakao alt="KAKAO Login" />,
 };
 
+export const KAKAO_OAUTH_LINKS: Record<OauthActions, string> = {
+  in: PATH_NAMES.KakaoSignIn,
+  up: PATH_NAMES.KakaoSignUp,
+};
+
 const OauthSignButton = ({ action, provider }: OauthSignButtonProps) => {
-  const OAUTH_LINK = {
+  const OAUTH_LINK: Record<OauthTypes, string> = {
     google: "",
-    kakao: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}/sign-${action}`,
+    kakao: KAKAO_OAUTH_LINKS[action],
   };
 
   return (
