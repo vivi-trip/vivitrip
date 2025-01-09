6487158900e47ae44685be2e29c8732d819af3bc
JuhyeokC <bos2ablues@gmail.com>
2025-01-09 19:26:47 +0900

    [feat/#53] 카카오 Oauth 타입 수정

--- src/types/oauth.ts
+++ src/types/oauth.ts
@@ -1,7 +1,7 @@
-import { UserNickName } from "@/src/types/user";
+export type OauthActions = "in" | "up";
 
 export interface OauthSignProps {
-  action: "in" | "up";
+  action: OauthActions;
 }
 
 export type OauthTypes = "google" | "kakao";
@@ -10,6 +10,11 @@ export interface OauthTypeProps {
   provider: OauthTypes;
 }
 
+export type OauthKaKaoProfile = {
+  nickname: string | "";
+  profileImageUrl: string | "";
+};
+
 export interface OauthSignButtonProps extends OauthSignProps, OauthTypeProps {}
 
 export interface OauthSignInProps extends OauthTypeProps {
@@ -17,4 +22,4 @@ export interface OauthSignInProps extends OauthTypeProps {
   token: string;
 }
 
-export interface OauthSignUpProps extends UserNickName, OauthSignInProps {}
+export interface OauthSignUpProps extends OauthKaKaoProfile, OauthSignInProps {}
