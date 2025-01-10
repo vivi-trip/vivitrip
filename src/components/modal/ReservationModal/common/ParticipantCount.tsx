6b078e194d4f7da6401acffe5db15e07c8082983
강효성 <goodkingdawn@naver.com>
2025-01-11 02:07:43 +0900

    [feat/#103]맴버 카운트 이슈 해결

--- src/components/modal/ReservationModal/common/ParticipantCount.tsx
+++ src/components/modal/ReservationModal/common/ParticipantCount.tsx
@@ -1,42 +1,41 @@
 import MinusIcon from "@/assets/svgs/ic_minus_btn.svg";
 import PlusIcon from "@/assets/svgs/ic_plus_btn.svg";
 import { useCalendarStore } from "@/src/stores/CalendarStore";
-import React from "react";
+import React, { useState } from "react";
 
-interface ParticipantCountProps {
-  count: number;
-}
-
-const ParticipantCount = ({ count }: ParticipantCountProps) => {
+const ParticipantCount = () => {
   const { onChangeMembers } = useCalendarStore();
 
+  const [members, setMembers] = useState(1);
+
   const handleCountPlus = () => {
-    onChangeMembers(count + 1);
+    const newMembers = members + 1;
+    setMembers(newMembers);
+    onChangeMembers(newMembers);
   };
+
   const handleCountMinus = () => {
-    onChangeMembers(count > 1 ? count - 1 : 1);
+    const newMembers = members > 1 ? members - 1 : 1;
+    setMembers(newMembers);
+    onChangeMembers(newMembers);
   };
 
   return (
     <div>
       <div className="mt-16 flex flex-col gap-8 border-t-2 pb-24 pt-12">
         <p className="font-18px-bold">참여 인원 수</p>
-        <div className="flex w-120 h-40 items-center rounded-md border bg-white">
+        <div className="flex h-40 w-120 items-center rounded-md border bg-white">
           <button
             type="button"
             className="p-10"
             onClick={handleCountMinus}
-            disabled={count <= 1}>
+            disabled={members <= 1}>
             <MinusIcon />
           </button>
           <div className="flex size-40 items-center justify-center p-8">
-            {count}
+            {members}
           </div>
-          <button
-            type="button"
-            className="p-10"
-            onClick={handleCountPlus}
-            disabled={count <= 1}>
+          <button type="button" className="p-10" onClick={handleCountPlus}>
             <PlusIcon />
           </button>
         </div>
