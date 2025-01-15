fe919d409100ccc1b1ace87488924f3736bf3d17
JuhyeokC <bos2ablues@gmail.com>
2025-01-15 18:07:14 +0900

    [feat/#122] 알림 기능 구현

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,25 +1,150 @@
+import IconClose from "@/assets/svgs/close.svg";
 import IconNotification from "@/assets/svgs/ic_notification.svg";
+import IconRefresh from "@/assets/svgs/ic_refresh.svg";
 import Dropdown from "@/src/components/Dropdown";
 import NotificationItem from "@/src/components/NotificationItem";
+import {
+  useDeleteMyNotification,
+  useMyNotificationsListQuery,
+} from "@/src/queries/my-notifications";
+import type {
+  MyNotificationsProps,
+  NotificationId,
+} from "@/src/types/my-notifications";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+
+/**
+ * @description 알림 목데이터
+ */
+const MOCK_DATA: MyNotificationsProps[] = [
+  {
+    id: 1001,
+    teamId: "팀 아이디 1",
+    userId: 1002,
+    content: "체험명(2023-01-14 15:00~18:00) 예약이 승인되었어요.",
+    createdAt: "2025-01-13T14:00:00",
+    updatedAt: "2025-01-13T16:30:00",
+    deletedAt: "2025-01-13T18:00:00",
+  },
+  {
+    id: 2001,
+    teamId: "팀 아이디 2",
+    userId: 2002,
+    content: "체험명(2023-01-14 15:00~18:00) 예약이 거절되었어요.",
+    createdAt: "2025-01-14T14:00:00",
+    updatedAt: "2025-01-14T16:30:00",
+    deletedAt: "2025-01-14T18:00:00",
+  },
+  {
+    id: 3001,
+    teamId: "팀 아이디 3",
+    userId: 3002,
+    content: "체험명(2023-01-14 15:00~18:00) 예약이 새로 들어왔어요.",
+    createdAt: "2025-01-15T14:00:00",
+    updatedAt: "2025-01-15T16:30:00",
+    deletedAt: "2025-01-15T18:00:00",
+  },
+];
 
 const Notification = () => {
+  const router = useRouter();
+  const size: number = 10;
+  const [cursorId, setCursorId] = useState<number | null>(null);
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const [items, setItems] = useState<MyNotificationsProps[]>([]);
+  const { data, isPending, refetch } = useMyNotificationsListQuery({
+    size,
+    cursorId,
+  });
+  const { mutate: deleteNotification } = useDeleteMyNotification();
+
+  const handleDeleteItem = async (id: NotificationId) => {
+    console.log("🚀 ~ handleDeleteItem ~ id:", id);
+    /**
+     * @todo
+     * 알림 삭제 함수 실행
+     */
+    const response = await deleteNotification({ notificationId: id });
+    console.log("🚀 ~ handleDeleteItem ~ response:", response);
+
+    setItems((prev) => {
+      return prev.filter((item) => item.id !== id);
+    });
+  };
+
   /**
    * @todo
-   * 알람 여부에 따라 인디케이터 출력
+   * 실제 알림 데이터로 구현 시 수정해야함.
    */
-  const Indicator = false;
+  useEffect(() => {
+    setTotalCount(items.length);
+  }, [items]);
+
+  const handleIndicator = useCallback(() => {
+    if (data) {
+      setCursorId(data.data.cursorId);
+      setTotalCount(MOCK_DATA.length);
+      // setTotalCount(data.data.totalCount);
+      // setItems(data.data.notifications);
+      setItems(MOCK_DATA);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  useEffect(() => {
+    handleIndicator();
+  }, [handleIndicator]);
+
+  useEffect(() => {
+    console.log("🚀 ~ Notification ~ router:", router);
+  }, [router]);
 
   return (
-    <Dropdown sustain>
+    <Dropdown sustain key={router.asPath}>
       <Dropdown.Trigger>
-        {Indicator && (
+        {totalCount > 0 && (
           <i className="absolute right-0 top-0 z-10 size-8 -translate-x-2 -translate-y-2 rounded-full border border-white bg-red-200" />
         )}
         <IconNotification className="text-gray-600" />
       </Dropdown.Trigger>
-      <Dropdown.Menu className="left-auto right-0 w-fit">
-        <Dropdown.Close>X</Dropdown.Close>
-        <NotificationItem />
+
+      <Dropdown.Menu className="left-auto right-0 w-fit !border-gray-200 shadow-xl">
+        <div className="fixed z-50 flex flex-col bg-brand-200 md:relative md:min-w-320">
+          <div className="flex items-center justify-between p-16 pb-8 text-brand-600 last:pb-16">
+            <div className="flex items-center justify-start gap-8">
+              <p className="font-20px-bold text-nowrap">
+                {totalCount > 0 ? `알림 ${totalCount}개` : `알림이 없습니다.`}
+              </p>
+              <button
+                type="button"
+                aria-label="새로고침"
+                onClick={() => refetch()}
+                disabled={isPending}>
+                <IconRefresh className="size-16" />
+              </button>
+            </div>
+            <Dropdown.Close>
+              <IconClose />
+            </Dropdown.Close>
+          </div>
+
+          {items && items.length > 0 && (
+            <div className="flex flex-col gap-8 overflow-auto p-16 pb-24 pt-8 md:max-h-320">
+              {items.map((item) => {
+                return (
+                  <NotificationItem
+                    key={`MyNotifications_${item.id}`}
+                    item={item}
+                    handleDelete={() => {
+                      handleDeleteItem(item.id);
+                    }}
+                  />
+                );
+              })}
+            </div>
+          )}
+        </div>
       </Dropdown.Menu>
     </Dropdown>
   );
