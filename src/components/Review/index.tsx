16ddd5d86061e4395221debae738dd20dc94a77b
hayuri1990 <hayuri1990@gmail.com>
2025-01-18 07:29:31 +0900

    [feat/#123] 리뷰 데이터 조회, 페이징 로직 및 UI 구성

---
+++ src/components/Review/index.tsx
@@ -0,0 +1,72 @@
+import Pagination from "@/src/components/Pagination";
+import ProgressBar from "@/src/components/Review/ProgressBar";
+import RatingSummary from "@/src/components/Review/RatingSummary";
+import ReviewList from "@/src/components/Review/ReviewList";
+import { getActivityReviews } from "@/src/services/activities";
+import { ReviewSummary } from "@/src/types/review";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const Review = ({ reviews, totalCount, averageRating }: ReviewSummary) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+  const [fetchedReviews, setFetchedReviews] = useState([]);
+  const activityId = reviews[0]?.activityId;
+  const size = 3;
+
+  // 초기 페이지 설정
+  useEffect(() => {
+    if (!searchParams.get("page")) {
+      router.replace(`?page=1`);
+    }
+  }, [searchParams, router]);
+
+  useEffect(() => {
+    if (totalCount === 0) return;
+
+    const fetchReviews = async () => {
+      try {
+        const page = Number(searchParams.get("page")) || 1;
+
+        const response = await getActivityReviews({ activityId, page, size });
+        setFetchedReviews(response.reviews);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    };
+
+    fetchReviews();
+  }, [activityId, reviews, searchParams, size, totalCount]);
+
+  return (
+    <div className="mx-auto flex max-w-1200 flex-col gap-24">
+      <header className="flex gap-10">
+        <h2 className="font-20px-bold">후기</h2>
+        <h2 className="font-20px-bold text-brand-500">{totalCount}</h2>
+      </header>
+      <article className="mb-24 flex h-180 items-center justify-evenly gap-10 overflow-hidden rounded-20 border border-gray-200 bg-neutral-100/40 px-10">
+        <RatingSummary
+          reviews={reviews}
+          totalCount={totalCount}
+          averageRating={averageRating}
+        />
+        <div className="h-full border-r border-gray-200" />
+        <ProgressBar reviews={reviews} totalCount={totalCount} />
+      </article>
+      <ReviewList fetchedReviews={fetchedReviews} />
+      <footer className="mb-120 mt-16 flex justify-center md:mt-66 lg:mt-48">
+        <Pagination
+          totalItems={totalCount}
+          pageCount={5}
+          currentPage={currentPage}
+          itemCountPerPage={size}
+        />
+      </footer>
+    </div>
+  );
+};
+
+export default Review;
