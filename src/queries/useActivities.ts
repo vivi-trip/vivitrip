6a480fac8eb7ccf50b1a3e7b2af742f7b1c4d91a
강효성 <goodkingdawn@naver.com>
2025-01-20 09:40:36 +0900

    [feat/#114] 체험 쿼리 훅  생성

---
+++ src/queries/useActivities.ts
@@ -0,0 +1,51 @@
+import {
+  createActivity,
+  createActivityImageUrl,
+  getActivity,
+} from "../services/activities";
+import { patchMyActivity } from "../services/myActiviesApi";
+import { ActivityImageUrl, CreateActivityProps } from "../types/activities";
+import { ActivityUpdateRequest } from "../types/activitiesReservationType";
+import { ActivityDetailResponse } from "../types/activitiesResponses";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useGetActivities = (activityId: number | undefined) => {
+  const { data, isLoading, error } = useQuery<ActivityDetailResponse>({
+    queryKey: ["activity", activityId],
+    queryFn: () => {
+      if (activityId === undefined) {
+        throw new Error("Activity ID is undefined");
+      }
+      return getActivity({ activityId });
+    },
+    enabled: activityId !== undefined,
+  });
+
+  return { data, isLoading, error };
+};
+
+
+export const useCreateActivity = () => {
+  return useMutation({
+    mutationFn: (data: CreateActivityProps) => createActivity(data),
+  });
+};
+
+export const usePatchMyActivity = () => {
+  return useMutation({
+    mutationFn: ({
+      activityId,
+      updateData,
+    }: {
+      activityId: number;
+      updateData: ActivityUpdateRequest;
+    }) => patchMyActivity(activityId, updateData),
+  });
+};
+
+export const useUploadActivtyAddImage = () => {
+  const { mutate } = useMutation<ActivityImageUrl, Error, File>({
+    mutationFn: (file: File) => createActivityImageUrl(file),
+  });
+  return { mutate };
+};
