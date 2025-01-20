54e1de107b355db853143fca4356a2787777b117
강효성 <goodkingdawn@naver.com>
2025-01-20 09:37:50 +0900

    [feat/#114] 내 체험 관리 페이지 구현

--- src/pages/my-activities/index.tsx
+++ src/pages/my-activities/index.tsx
@@ -1,10 +1,38 @@
+import Button from "@/src/components/Button/Button";
+import ActivityList from "@/src/components/MyAtivities/ActivityList";
 import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
-
+import { useRouter } from "next/router";
 
 const MyActivities = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/my-activities/registration");
+  };
   return (
     <div>
-      <SideNavigationMenu />
+      <div className="flex w-full justify-center gap-24 pt-72">
+        <SideNavigationMenu />
+        <div className="flex-1">
+          <div className="flex justify-between">
+            <p className="font-32px-bold">내 체험 관리</p>
+            <Button
+              type="button"
+              width="120"
+              height="48"
+              radius="4"
+              gap="4"
+              fontStyle="l"
+              backgroundColor="black"
+              onClick={handleClick}>
+              체험 등록하기
+            </Button>
+          </div>
+          <div className="">
+            <ActivityList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
