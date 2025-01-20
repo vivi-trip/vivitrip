3e4173a6beb09eadca7b720c187ca4fd900f906f
강효성 <goodkingdawn@naver.com>
2025-01-20 09:48:52 +0900

    [fix/#114] 시간 선택 컴포넌트 일부 코드 주석처리

--- src/components/modal/ReservationModal/common/TimeSelector.tsx
+++ src/components/modal/ReservationModal/common/TimeSelector.tsx
@@ -1,10 +1,14 @@
 import Button from "@/src/components/Button/Button";
-import { useCalendarStore } from "@/src/stores/CalendarStore";
-import { Schedule } from "@/src/types/schedule";
+import { useCalendar } from "@/src/stores/CalendarStore";
+import { Schedule } from "@/src/types/activitiesResponses";
 import React from "react";
 
-const TimeSelector = (schedules) => {
-  const { onChangeSchedule, selectSchedule, formatDate } = useCalendarStore();
+/**
+ * @todo 프롭으로 받는 schedules 추가 작업
+ *
+ */
+const TimeSelector = () => {
+  const { onChangeSchedule, selectSchedule, formatDate } = useCalendar();
 
   const handleIsDateButtons = (data: Schedule) => {
     const isSelected = selectSchedule?.id === data.id;
@@ -30,21 +34,21 @@ const TimeSelector = (schedules) => {
     return <div />;
   };
 
-  const renderSchedules = () => {
-    const filter = schedules.find((v) => v.date === formatDate);
-    if (!filter) {
-      return (
-        <div className="font-18px-medium text-basic-black">
-          예약가능한 시간이 없습니다.
-        </div>
-      );
-    }
-    return schedules.map((data) => handleIsDateButtons(data));
-  };
+  // const renderSchedules = () => {
+  //   const filter = schedules.find((v) => v.date === formatDate);
+  //   if (!filter) {
+  //     return (
+  //       <div className="font-18px-medium text-basic-black">
+  //         예약가능한 시간이 없습니다.
+  //       </div>
+  //     );
+  //   }
+  //   return schedules.map((data) => handleIsDateButtons(data));
+  // };
   return (
     <div>
       <p className="font-18px-bold">예약 가능한 시간</p>
-      <div>{renderSchedules}</div>
+      {/* <div>{renderSchedules}</div> */}
     </div>
   );
 };
