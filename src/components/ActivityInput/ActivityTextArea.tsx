e9ac235e9f28fe8a76e35a487b399a447916437b
강효성 <goodkingdawn@naver.com>
2025-01-20 09:33:39 +0900

    [feat/#114] 체험 내용 입력 인풋 구현

---
+++ src/components/ActivityInput/ActivityTextArea.tsx
@@ -0,0 +1,41 @@
+import { ActivityFormDataType } from "@/src/types/activityFormDataType";
+import clsx from "clsx";
+import React from "react";
+import { Control, Controller } from "react-hook-form";
+
+interface TextAreaProps {
+  control: Control<ActivityFormDataType>;
+}
+
+const ActivityTextArea = ({ control }: TextAreaProps) => {
+  return (
+    <div>
+      <Controller
+        name="description"
+        control={control}
+        rules={{
+          required: { value: true, message: "필수 입력 값입니다." },
+          pattern: {
+            value: /^(?=.*[A-Za-z가-힣])[\dA-Za-z가-힣\s]+$/,
+            message: "문자열을 포함하여 작성해주세요.",
+          },
+        }}
+        render={({ field, fieldState: { error } }) => (
+          <>
+            <textarea
+              {...field}
+              placeholder="설명"
+              className={clsx(
+                "font-16px-regular w-full rounded-4 border border-gray-500 py-15 pl-16",
+                "min-h-346 resize-none",
+              )}
+            />
+            {error && <div className="pl-1 text-red-500">{error?.message}</div>}
+          </>
+        )}
+      />
+    </div>
+  );
+};
+
+export default ActivityTextArea;
