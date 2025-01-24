76654e2a2071b3ef4b40d92edf310db22f4794d0
hayuri1990 <hayuri1990@gmail.com>
2025-01-24 11:42:27 +0900

    [feat/#133] Pagination 버튼에 로딩 스피너 적용

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,14 +1,24 @@
 import AltArrowLeft from "@/assets/svgs/AltArrowLeft.svg";
 import AltArrowRight from "@/assets/svgs/AltArrowRight.svg";
 import Button from "@/src/components/Button/Button";
+import Loading from "@/src/components/Loading";
 import usePagination from "@/src/hooks/usePagination";
+import useLoadingStore from "@/src/stores/loadingStore";
 import { PaginationProps } from "@/src/types/pagination";
 
+/**
+ * @description
+ * @param totalItems - 데이터 총 개수
+ * @param pageCount -  한 번에 보여줄 페이지 개수
+ * @param currentPage -  현재 페이지
+ * @param itemCountPerPage -  페이지 당 데이터 개수
+ */
+
 const Pagination = ({
-  totalItems, // 데이터 총 개수
-  pageCount, // 한 번에 보여줄 페이지 개수
-  currentPage, // 현재 페이지
-  itemCountPerPage, // 페이지 당 데이터 개수
+  totalItems,
+  pageCount,
+  currentPage,
+  itemCountPerPage,
 }: PaginationProps) => {
   const { start, noPrev, noNext, navigateToPage, totalPages } = usePagination({
     totalItems,
@@ -18,6 +28,30 @@ const Pagination = ({
   });
   const buttonSize = "w-40 h-40 sm:w-55 sm:h-55";
   const iconSize = "w-21 h-21";
+
+  // loading spinner
+  const { loadingButtons, showLoadingButtons, hideLoadingButtons } =
+    useLoadingStore();
+
+  const handlePageClick = async (pageNumber: number) => {
+    showLoadingButtons(pageNumber);
+    try {
+      await navigateToPage(pageNumber);
+    } finally {
+      hideLoadingButtons(pageNumber);
+    }
+  };
+
+  const loadingSpinner = (
+    <Loading
+      isOverlay="false"
+      overlayColor="translate"
+      isAbsolute="false"
+      loadingBoxColor="translate"
+      color="#ffffff"
+      size={35}
+    />
+  );
 
   return (
     <div>
@@ -29,12 +63,16 @@ const Pagination = ({
             gap="10"
             backgroundColor={noPrev ? "white_gray" : "white_green"}
             disabled={noPrev}
-            className={`${buttonSize}`}
-            onClick={() => navigateToPage(start - 1)}>
-            <AltArrowLeft
-              className={`${iconSize} ${noPrev ? "text-gray-600" : "text-brand-400"}`}
-              aria-label="이전 페이지로 이동 버튼"
-            />
+            className={`${buttonSize} relative`}
+            onClick={() => handlePageClick(start - 1)}>
+            {loadingButtons?.[start - 1] ? (
+              loadingSpinner
+            ) : (
+              <AltArrowLeft
+                className={`${iconSize} ${noPrev ? "text-gray-600" : "text-brand-400"}`}
+                aria-label="이전 페이지로 이동 버튼"
+              />
+            )}
           </Button>
         </li>
         {Array.from({ length: pageCount }).map((_, i) => {
@@ -52,8 +90,8 @@ const Pagination = ({
                   fontStyle={isActivePage ? "xxxl" : "xxl"}
                   disabled={isActivePage}
                   className={`${buttonSize}`}
-                  onClick={() => navigateToPage(pageNumber)}>
-                  {pageNumber}
+                  onClick={() => handlePageClick(pageNumber)}>
+                  {loadingButtons?.[pageNumber] ? loadingSpinner : pageNumber}
                 </Button>
               </li>
             )
@@ -67,11 +105,15 @@ const Pagination = ({
             backgroundColor={noNext ? "white_gray" : "white_green"}
             className={`${buttonSize}`}
             disabled={noNext}
-            onClick={() => navigateToPage(start + pageCount)}>
-            <AltArrowRight
-              className={`${iconSize} ${noNext ? "text-gray-600" : "text-brand-400"}`}
-              aria-label="다음 페이지로 이동 버튼"
-            />
+            onClick={() => handlePageClick(start + pageCount)}>
+            {loadingButtons?.[start + pageCount] ? (
+              loadingSpinner
+            ) : (
+              <AltArrowRight
+                className={`${iconSize} ${noNext ? "text-gray-600" : "text-brand-400"}`}
+                aria-label="다음 페이지로 이동 버튼"
+              />
+            )}
           </Button>
         </li>
       </ul>
