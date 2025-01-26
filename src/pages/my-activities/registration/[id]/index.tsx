6beb5bbae408a050d989e15019e60abd8f86a362
강효성 <goodkingdawn@naver.com>
2025-01-27 03:35:15 +0900

    [feat/#114]  유저 확인 로직 추가 - 현재 오류 발생 확인 필요

--- src/pages/my-activities/registration/[id]/index.tsx
+++ src/pages/my-activities/registration/[id]/index.tsx
@@ -1,5 +1,7 @@
 import ActivityConservation from "@/src/components/MyAtivities/ActivityConservation";
 import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
+import { useGetActivities } from "@/src/queries/useActivities";
+import useUserStore from "@/src/stores/userStore";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -7,13 +9,34 @@ const RegisterModify = () => {
   const router = useRouter();
   const [activityId, setActivityId] = useState<number | undefined>(undefined);
 
+  const { userData } = useUserStore();
+
   useEffect(() => {
-    if (router.isReady) {
-      const numericId = Number(router.query.id);
+    if (!router.isReady) return;
+
+    const numericId = Number(router.query.id);
+    if (!Number.isNaN(numericId)) {
       setActivityId(numericId);
+    } else {
+      setActivityId(undefined);
     }
   }, [router.isReady, router.query.id]);
 
+  const { data: activitiesDetail } = useGetActivities(activityId);
+
+  useEffect(() => {
+    if (!userData) {
+      // 로그인하지 않은 경우, 로그인 페이지로 리다이렉트
+      router.push("/sign-in");
+      return;
+    }
+
+    if (activitiesDetail && activitiesDetail.userId !== userData.id) {
+      // 활동의 소유자가 아닌 경우, 메인 페이지나 다른 페이지로 리다이렉트
+      router.push("/home");
+    }
+  }, [activitiesDetail, userData, router]);
+  
   return (
     <div>
       <div className="flex w-full min-w-343 justify-center gap-24 pt-72">
