42df5b02efc6bf3e037faa7bb2b4ffaa130a343b
강효성 <goodkingdawn@naver.com>
2025-01-27 03:35:46 +0900

    [fix/#114]  타입  주석  추가

--- src/types/activitiesResponses.ts
+++ src/types/activitiesResponses.ts
@@ -1,8 +1,20 @@
+/**
+ * @description 서브이미지
+ * @param id 서브이미지 id
+ * @param imageUrl 서브이미지 url
+ */
 export interface SubImage {
   id?: number;
   imageUrl: string;
 }
 
+/**
+ * @description 체험 시단대별 스케쥴
+ * @param id 체험 시간대별 id
+ * @param date 체험 날짜
+ * @param startTime 체험 시작시간
+ * @param endTime 체험 종료시간
+ */
 export interface Schedule {
   id?: number;
   date: string;
@@ -10,6 +22,14 @@ export interface Schedule {
   endTime: string;
 }
 
+/**
+ * @description 체험 기본데이터 타입
+ * @param title 체험 타이틀
+ * @param description 체험 내용
+ * @param category 체험 카테고리
+ * @param price 체험 인당 가격
+ * @param address 체험 주소
+ */
 export interface ActivityBasicDataType {
   title: string;
   description: string;
@@ -18,6 +38,18 @@ export interface ActivityBasicDataType {
   address: string;
 }
 
+/**
+ * @description 체험 상세 리스폰스 데이터
+ * @param id 체험 id
+ * @param userId 유저 id
+ * @param bannerImageUrl 배너 이미지 url
+ * @param subImages 서비이미지 배열
+ * @param schedules 체험 시간대별 id
+ * @param reviewCount 등록된 리뷰수
+ * @param rating 별점
+ * @param createdAt 체험 생성 시간
+ * @param updatedAt 체험 업데이트 시간
+ */
 export interface ActivityDetailResponse extends ActivityBasicDataType {
   id: number;
   userId: number;
--- src/types/activityFormDataType.ts
+++ src/types/activityFormDataType.ts
@@ -6,14 +6,8 @@ import {
 
 /**
  * @description 폼데이터 타입
- * @param title - 체험제목
- * @param category - 체험 카테고리
- * @param description - 체험 설명
- * @param address - 체험 주소
- * @param price - 체험 가격
  * @param bannerImageUrl - 배너 이미지
- * @param subImageUrls - 소개 이미지
- * @param scheduleIdsToRemove - 스케줄 삭제
+ * @param subImages - 소개 이미지
  * @param schedules - 체험 날짜 | 시작시간 | 종료시간
  */
 export interface ActivityFormDataType extends ActivityBasicDataType {
