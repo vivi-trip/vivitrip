4ed875b72ff70e1fa9da57e8da89692a156e6ab2
강효성 <goodkingdawn@naver.com>
2025-01-27 03:34:26 +0900

    [feat/#114]  유저 확인  로직 추가

--- src/pages/my-activities/registration/index.tsx
+++ src/pages/my-activities/registration/index.tsx
@@ -1,7 +1,20 @@
 import ActivityConservation from "@/src/components/MyAtivities/ActivityConservation";
 import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
+import useUserStore from "@/src/stores/userStore";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Registration = () => {
+  const { userData } = useUserStore();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!userData) {
+      router.push("/sign-in");
+    }
+  }, [userData, router]);
+
   return (
     <div>
       <div className="flex w-full min-w-343 justify-center gap-24 pt-72">
