5a1370f234d2f33416cd07de7a452bf46a66b040
강효성 <goodkingdawn@naver.com>
2025-01-27 03:31:59 +0900

    [fix/#114] 체험 카드 정렬 수정

--- src/components/MyAtivities/ActivityCard.tsx
+++ src/components/MyAtivities/ActivityCard.tsx
@@ -80,60 +80,66 @@ const ActivitiesCard = ({ activity }: ActivitiesCardProps) => {
               src={bannerImageUrl}
               alt="체험 이미지"
               fill
-              object-fit="cover"
-              className="rounded-l-24 bg-brand-400"
+              objectFit="cover"
+              className="rounded-l-24 border border-brand-200 bg-brand-400"
             />
           </div>
-          <div className="my-9 ml-8 flex flex-col gap-6">
-            <div className="flex gap-6">
+          <div
+            className={clsx(
+              "my-9 ml-8 mr-14 flex w-full flex-col",
+              "md:m-12 md:mr-18 md:gap-6 lg:my-14 lg:ml-24",
+            )}>
+            <div className="flex items-center gap-6">
               <IconStar />
               <span className="font-14px-regular md:font-16px-regular">
                 {rating} ({reviewCount})
               </span>
             </div>
-            <div className="flex h-full flex-col justify-between">
+            <div className="flex h-full flex-col items-start justify-between">
               <p className="font-14px-bold md:font-20px-bold">{title}</p>
-              <p className="font-16px-medium md:font-24px-medium">
-                ₩{price?.toLocaleString("ko-KR")} /인
-              </p>
+              <div className="flex w-full items-center justify-between">
+                <p className="font-16px-medium md:font-20px-medium lg:font-24px-medium lg:text-gray-700">
+                  ₩{price?.toLocaleString("ko-KR")} /인
+                </p>
+                <div>
+                  <Dropdown className="flex">
+                    <Dropdown.Trigger className="flex justify-end">
+                      <div className="hidden md:block">
+                        <IconKebab />
+                      </div>
+                      <div className="block md:hidden">
+                        <IconKebabSmall />
+                      </div>
+                    </Dropdown.Trigger>
+                    <Dropdown.Menu className="-left-110">
+                      <Dropdown.Item
+                        onClick={handleMoveToEditPage}
+                        className="px-48 py-17">
+                        수정하기
+                      </Dropdown.Item>
+                      <div className="border-b border-gray-200" />
+                      <Dropdown.Item
+                        onClick={() => {
+                          setModalOpen(
+                            <TwoButtonModal
+                              title="체험을 삭제하시겠습니까?"
+                              negativeContent="취소하기"
+                              interactiveContent="삭제하기"
+                              onCancel={handleDeleteData}
+                            />,
+                          );
+                        }}
+                        className="px-48 py-17">
+                        삭제하기
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </button>
-      <div className="absolute bottom-14 right-24 z-10">
-        <Dropdown className="flex w-160">
-          <Dropdown.Trigger className="flex justify-end">
-            <div className="hidden md:block">
-              <IconKebab />
-            </div>
-            <div className="block md:hidden">
-              <IconKebabSmall />
-            </div>
-          </Dropdown.Trigger>
-          <Dropdown.Menu>
-            <Dropdown.Item
-              onClick={handleMoveToEditPage}
-              className="px-48 py-17">
-              수정하기
-            </Dropdown.Item>
-            <div className="border-b border-gray-200" />
-            <Dropdown.Item
-              onClick={() => {
-                setModalOpen(
-                  <TwoButtonModal
-                    title="체험을 삭제하시겠습니까?"
-                    negativeContent="취소하기"
-                    interactiveContent="삭제하기"
-                    onCancel={handleDeleteData}
-                  />,
-                );
-              }}
-              className="px-48 py-17">
-              삭제하기
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
     </div>
   );
 };
