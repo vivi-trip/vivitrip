fa298e0203c93b897aa64af3db4e791e361e8efd
강효성 <goodkingdawn@naver.com>
2025-02-03 14:15:57 +0900

    [feat/#148]예약내역 필터 드롭다운 구현

---
+++ src/components/MyReservations/ReservationStatusDropdown.tsx
@@ -0,0 +1,74 @@
+import Dropdown from "../Dropdown";
+import AltArrowDown from "@/assets/svgs/altArrowDown.svg";
+import { ReservationStatus } from "@/src/types/my-reservations";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
+interface ReservationStatusDropdownProps {
+  handleStatusChange: (status: ReservationStatus) => void;
+}
+
+const ReservationStatusDropdown = ({
+  handleStatusChange,
+}: ReservationStatusDropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [status, setStatus] = useState<ReservationStatus | "">("");
+
+  const handleItemClick = (newStatus: ReservationStatus) => {
+    setStatus(newStatus);
+
+    setIsOpen(false);
+  };
+
+  const options = [
+    { label: "예약 신청", value: "pending" },
+    { label: "예약 취소", value: "canceled" },
+    { label: "예약 승인", value: "confirmed" },
+    { label: "예약 거절", value: "declined" },
+    { label: "체험 완료", value: "completed" },
+  ];
+
+  useEffect(() => {
+    if (status !== "") {
+      handleStatusChange(status);
+    }
+  }, [status, handleStatusChange]);
+
+  return (
+    <div>
+      <Dropdown>
+        <Dropdown.Trigger
+          className={clsx(
+            "h-44 min-w-120 px-12 py-10",
+            "md:h-56 md:px-16 md:py-15 lg:w-160",
+            "rounded-15 border border-brand-400",
+            "flex justify-between",
+          )}
+          onClick={() => setIsOpen((prev) => !prev)}>
+          <p className="font-18px-medium text-brand-400">
+            {status
+              ? options.find((opt) => opt.value === status)?.label
+              : "필터"}
+          </p>
+          <AltArrowDown className={clsx(isOpen && "rotate-180")} />
+        </Dropdown.Trigger>
+        {isOpen && (
+          <Dropdown.Menu className="h-290 overflow-y-auto">
+            {options.map((option) => (
+              <Dropdown.Item
+                key={option.value}
+                onClick={() =>
+                  handleItemClick(option.value as ReservationStatus)
+                }
+                className="px-16 py-15 hover:bg-gray-100">
+                {option.label}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        )}
+      </Dropdown>
+    </div>
+  );
+};
+
+export default ReservationStatusDropdown;
