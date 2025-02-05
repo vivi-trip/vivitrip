d58ad5bec7f4f626b660ca6fcecffb72150af309
강효성 <goodkingdawn@naver.com>
2025-02-06 00:17:02 +0900

    [feat/#148] 리뷰 모달 api 적용

--- src/components/modal/ReviewModal/ReviewModal.tsx
+++ src/components/modal/ReviewModal/ReviewModal.tsx
@@ -2,27 +2,44 @@ import ReviewContent from "./common/ReviewContent";
 import ReviewForm from "./common/ReviewForm";
 import CloseIcon from "@/assets/svgs/btnXbig.svg";
 import useModalStore from "@/src/stores/ModalStore";
+import { ReviewData } from "@/src/types/my-reservatios-responses";
+import { formatDate3 } from "@/src/utils/calendarFormatDate";
+import clsx from "clsx";
 
-const ReviewModal = () => {
+interface ReviewModalProps {
+  riviewData: ReviewData;
+}
+
+const ReviewModal = ({ riviewData }: ReviewModalProps) => {
   const { setModalClose } = useModalStore();
+  const {
+    bannerImageUrl,
+    title,
+    date,
+    startTime,
+    endTime,
+    headCount,
+    totalPrice,
+    id: reservationId,
+  } = riviewData;
 
   return (
-    <div className="flex max-h-686 w-full max-w-432 flex-col items-center">
+    <div className={clsx("flex flex-col items-center")}>
       <div className="flex w-full justify-between">
         <h1 className="font-24px-bold text-black">후기 작성</h1>
         <CloseIcon onClick={setModalClose} />
       </div>
       <div className="mt-41 flex flex-col gap-24">
         <ReviewContent
-          title="[예시] 함께 배우면 즐거운 스트릿 댄스"
-          bannerImageUrl="/images/test_img.png"
-          date="2024.12.25"
-          startTime="11:00"
-          endTime="12:00"
-          headCount={2}
-          totalPrice={10000}
+          title={title}
+          bannerImageUrl={bannerImageUrl}
+          date={formatDate3(date)}
+          startTime={startTime}
+          endTime={endTime}
+          headCount={headCount}
+          totalPrice={totalPrice}
         />
-        <ReviewForm />
+        <ReviewForm reservationId={reservationId} />
       </div>
     </div>
   );
