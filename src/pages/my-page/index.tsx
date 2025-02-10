c797d4c19892e8e630564711bcc11d35cb7e478e
강효성 <goodkingdawn@naver.com>
2025-02-10 14:12:48 +0900

    [fix/#148] mypage 프로필 이미지 상태 추가 및 사이드 네비게이션 추가

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -1,13 +1,14 @@
 import Logo from "@/src/components/Logo";
 import MyPage from "@/src/components/MyPage";
 import MyPageKakao from "@/src/components/MyPage/kakao";
+import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
 import PATH_NAMES from "@/src/constants/pathname";
 import useHydration from "@/src/hooks/useHydration";
 import { useUpdateMyData } from "@/src/queries/auth";
 import useUserStore from "@/src/stores/userStore";
 import { UserPatchProps } from "@/src/types/user";
 import { useRouter } from "next/router";
-import { FormEvent, useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 const RouteMyPage = () => {
   const router = useRouter();
@@ -15,11 +16,19 @@ const RouteMyPage = () => {
   const { userData, userProvider } = useUserStore.getState();
   const { mutate, isPending } = useUpdateMyData();
 
+  const [imageUrl, setImageUrl] = useState("");
+
+  const onChangeImageURL = (newImageUrl: string) => {
+    setImageUrl(newImageUrl); // 이미지 URL 업데이트 함수
+  };
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
     const param = Object.fromEntries(formData) as unknown as UserPatchProps;
+
+    param.profileImageUrl = imageUrl;
 
     if (userProvider === "kakao") {
       param.newPassword = userData?.email
@@ -51,7 +60,16 @@ const RouteMyPage = () => {
       return <MyPageKakao handleSubmit={handleSubmit} isPending={isPending} />;
     }
     default: {
-      return <MyPage handleSubmit={handleSubmit} isPending={isPending} />;
+      return (
+        <div className="flex w-full justify-center gap-24 pt-72">
+          <div className="hidden md:block">
+            <SideNavigationMenu url={onChangeImageURL} />
+          </div>
+          <div className="md:flex-1">
+            <MyPage handleSubmit={handleSubmit} isPending={isPending} />
+          </div>
+        </div>
+      );
     }
   }
 };
