0e448a64fb60886fa9a38baaad9b5b4e6ba7a56a
강효성 <goodkingdawn@naver.com>
2025-02-10 14:19:33 +0900

    [feat/#148] 체험 예약 가능 시간 구현

--- src/components/modal/ReservationModal/common/TimeSelector.tsx
+++ src/components/modal/ReservationModal/common/TimeSelector.tsx
@@ -1,14 +1,23 @@
 import Button from "@/src/components/Button/Button";
 import { useCalendar } from "@/src/stores/CalendarStore";
-import { Schedule } from "@/src/types/activitiesResponses";
-import React from "react";
+import { Schedule, Schedules } from "@/src/types/activitiesResponses";
+import clsx from "clsx";
+import React, { useState } from "react";
 
-/**
- * @todo 프롭으로 받는 schedules 추가 작업
- *
- */
-const TimeSelector = () => {
+
+const TimeSelector = ({ schedules }: Schedules) => {
   const { onChangeSchedule, selectSchedule, formatDate } = useCalendar();
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
+  const handleButtonClick = (data: Schedule) => {
+    if (selectedId === data.id) {
+      setSelectedId(null);
+      onChangeSchedule(null);
+    } else {
+      setSelectedId(data.id);
+      onChangeSchedule(data);
+    }
+  };
 
   const handleIsDateButtons = (data: Schedule) => {
     const isSelected = selectSchedule?.id === data.id;
@@ -22,11 +31,12 @@ const TimeSelector = () => {
           radius="8"
           gap="10"
           fontStyle="xxxl"
-          className={`px-12 py-10 ${
-            isSelected ? "bg-brand-500 text-white" : "bg-white text-brand-500"
-          } `}
+          className={clsx("mt-16 h-46 px-12 py-10", {
+            "bg-brand-500 text-white": isSelected,
+            "border border-brand-500 text-brand-500": !isSelected,
+          })}
           key={data.id}
-          onClick={() => onChangeSchedule(data)}>
+          onClick={() => handleButtonClick(data)}>
           {data.startTime}~{data.endTime}
         </Button>
       );
@@ -34,21 +44,20 @@ const TimeSelector = () => {
     return <div />;
   };
 
-  // const renderSchedules = () => {
-  //   const filter = schedules.find((v) => v.date === formatDate);
-  //   if (!filter) {
-  //     return (
-  //       <div className="font-18px-medium text-basic-black">
-  //         예약가능한 시간이 없습니다.
-  //       </div>
-  //     );
-  //   }
-  //   return schedules.map((data) => handleIsDateButtons(data));
-  // };
+  const renderSchedules = () => {
+    const filter = schedules.find((v) => v.date === formatDate);
+    if (!filter) {
+      return (
+        <div className="font-16px-bold my-10">예약가능한 시간이 없습니다</div>
+      );
+    }
+    return schedules.map((data) => handleIsDateButtons(data));
+  };
+
   return (
     <div>
       <p className="font-18px-bold">예약 가능한 시간</p>
-      {/* <div>{renderSchedules}</div> */}
+      <div>{renderSchedules()}</div>
     </div>
   );
 };
