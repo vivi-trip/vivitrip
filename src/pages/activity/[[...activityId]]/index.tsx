e3a4a0f3d056cacb83af10958318e8a29c9401a2
JuhyeokC <bos2ablues@gmail.com>
2025-02-13 04:14:31 +0900

    [feat/#159] 체험 상세 조회 API 연결

--- src/pages/activity/[[...activityId]]/index.tsx
+++ src/pages/activity/[[...activityId]]/index.tsx
@@ -2,20 +2,63 @@ import IcKebab from "@/assets/svgs/ic_kebab.svg";
 import IcLocation from "@/assets/svgs/ic_location.svg";
 import IcStar from "@/assets/svgs/star.svg";
 import Dropdown from "@/src/components/Dropdown";
+import Loading from "@/src/components/Loading";
+import { useScroll } from "@/src/contexts/ScrollContext";
+import { useGetActivities } from "@/src/queries/useActivities";
+import { ActivityDetailResponse } from "@/src/types/activitiesResponses";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 const ActivitiesPage = () => {
   const router = useRouter();
   const { activityId } = router.query;
+  const {
+    data: activityData,
+    isLoading,
+    error,
+  } = useGetActivities(Number(activityId));
+
+  if (isLoading)
+    return (
+      <Loading
+        isOverlay="window"
+        overlayColor="blue"
+        isAbsolute="static"
+        loadingBoxColor="black"
+        size={{ sm: 50, md: 60, lg: 70 }}
+        loadingText="잠시만 기다려주세요."
+        textStyle="font-18px-medium md:font-20px-regular lg:font-24px-regular"
+        textColor="text-brand-50"
+        className="p-30"
+      />
+    );
+
+  if (error)
+    return (
+      <div>
+        <p>데이터를 가져오는 중 에러가 발생하였습니다.</p>
+        <p>잠시후 다시 시도해주세요.</p>
+      </div>
+    );
+
+  const {
+    category,
+    title,
+    address,
+    rating,
+    reviewCount,
+    description,
+    bannerImageUrl,
+    subImages,
+    userId,
+  } = activityData as ActivityDetailResponse;
 
   return (
     <div className="my-80 pb-300">
-      <p>체험 상세 페이지 activityId: {activityId}</p>
-
       <div className="flex items-center justify-between">
         <div>
-          <p className="font-14px-regular">문화·예술</p>
-          <p className="font-32px-bold mt-10">함께 배우면 즐거운 스트릿 댄스</p>
+          <p className="font-14px-regular">{category}</p>
+          <p className="font-32px-bold mt-10">{title}</p>
           <div className="mt-16 flex gap-12">
             <div className="flex items-center gap-6">
               <IcStar />
@@ -43,50 +86,44 @@ const ActivitiesPage = () => {
 
       <div className="mt-24 grid h-560 grid-cols-4 grid-rows-2 gap-8 overflow-hidden rounded-12">
         <div className="col-span-2 row-span-full size-full overflow-hidden">
-          <div className="font-32px-bold flex size-full items-center justify-center bg-brand-200 text-brand-600">
+          <div className="font-32px-bold relative flex size-full items-center justify-center bg-brand-200 text-brand-600">
             Image 1
+            <Image
+              src={bannerImageUrl}
+              alt="배너 이미지"
+              priority
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
           </div>
         </div>
-        <div className="size-full overflow-hidden">
-          <div className="font-32px-bold flex size-full items-center justify-center bg-brand-300 text-brand-600">
-            Image 2
-          </div>
-        </div>
-        <div className="size-full overflow-hidden">
-          <div className="font-32px-bold flex size-full items-center justify-center bg-brand-400 text-brand-50">
-            Image 3
-          </div>
-        </div>
-        <div className="size-full overflow-hidden">
-          <div className="font-32px-bold flex size-full items-center justify-center bg-brand-500 text-brand-50">
-            Image 4
-          </div>
-        </div>
-        <div className="size-full overflow-hidden">
-          <div className="font-32px-bold flex size-full items-center justify-center bg-brand-600 text-brand-50">
-            Image 5
-          </div>
-        </div>
+
+        {Array.from({ length: 4 }).map((_, i) => {
+          return (
+            <div
+              className="size-full overflow-hidden"
+              key={`subImages_${i + 1}`}>
+              <div className="font-32px-bold relative flex size-full items-center justify-center bg-brand-300 text-brand-600">
+                {subImages.length > i && (
+                  <Image
+                    src={subImages[i].imageUrl ?? ""}
+                    alt="서브 이미지"
+                    priority
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
       {/* 체험 이미지 영역 */}
 
       <div className="mt-80 grid grid-cols-8 grid-rows-3 gap-40">
         <div className="col-span-5 col-start-1 border-t border-brand-300 pt-40">
           <p className="font-20px-bold">체험 설명</p>
-          <p className="font-16px-regular mt-16">
-            안녕하세요! 저희 스트릿 댄스 체험을 소개합니다. 저희는 신나고
-            재미있는 스트릿 댄스 스타일을 가르칩니다. 크럼프는 세계적으로 인기
-            있는 댄스 스타일로, 어디서든 춤출 수 있습니다. 저희 체험에서는
-            새로운 스타일을 접할 수 있고, 즐거운 시간을 보낼 수 있습니다. 저희는
-            초보자부터 전문가까지 어떤 수준의 춤추는 사람도 가르칠 수 있도록
-            준비해놓았습니다. 저희와 함께 즐길 수 있는 시간을 기대해주세요! 각종
-            음악에 적합한 스타일로, 저희는 크럼프 외에도 전통적인 스트릿 댄스
-            스타일과 최신 스트릿 댄스 스타일까지 가르칠 수 있습니다. 저희
-            체험에서는 전문가가 직접 강사로 참여하기 때문에, 저희가 제공하는
-            코스는 어떤 수준의 춤추는 사람도 쉽게 이해할 수 있도록
-            준비해놓았습니다. 저희 체험을 참가하게 된다면, 즐거운 시간 뿐만
-            아니라 새로운 스타일을 접할 수 있을 것입니다.
-          </p>
+          <p className="font-16px-regular mt-16">{description}</p>
         </div>
         {/* 설명 영역 */}
 
