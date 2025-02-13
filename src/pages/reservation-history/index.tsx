47371ebdcba35cdb63a23ecf3ad696cf1be38a31
강효성 <goodkingdawn@naver.com>
2025-02-13 15:06:13 +0900

    [fix/#135]불필요한 flex 제거

--- src/pages/reservation-history/index.tsx
+++ src/pages/reservation-history/index.tsx
@@ -21,73 +21,69 @@ const MyReservationHistory = () => {
 
   return (
     <div>
-      <div className="mb-120 flex w-full justify-center gap-24 pt-72">
+      <div className="mb-120 flex w-full min-w-320 justify-center gap-24 pt-72">
         <SideNavigationMenu />
         <div className="flex-1">
-          <div className="flex flex-col">
-            <p className="font-32px-bold">예약 현황</p>
-            {myActivities ? (
-              <div className="mt-32 flex flex-col">
-                <div className="relative mb-24 lg:mb-30">
-                  <p className="font-14px-regular absolute left-12 top-[-10px] z-10 bg-gray-50 px-4">
-                    체험명
-                  </p>
-                  <Dropdown>
-                    <Dropdown.Trigger
-                      onClick={() => {
-                        setIsOpen(true);
-                      }}
-                      className={clsx(
-                        "font-16px-regular text-left",
-                        "flex h-56 w-full items-center justify-between",
-                        "rounded-4 border border-gray-500",
-                        "px-16 py-15",
-                      )}>
-                      {activityTitle || "체험을 선택하세요"}
-                      {isOpen ? <IconArrowUp /> : <IconArrowDown />}
-                    </Dropdown.Trigger>
-                    <Dropdown.Menu className="w-full">
-                      {myActivities && myActivities.length > 0 ? (
-                        myActivities.map((activity) => (
-                          <Dropdown.Item
-                            key={activity.id}
-                            className="font-16px-regular flex justify-start py-15 pl-16 text-black hover:bg-gray-100"
-                            onClick={() => {
-                              setSelectedActivityId(activity.id);
-                              setActivityTitle(activity.title);
-                              setIsOpen(false);
-                            }}>
-                            {activity.title}
-                          </Dropdown.Item>
-                        ))
-                      ) : (
-                        <Dropdown.Item className="font-16px-regular flex justify-start py-15 pl-16 text-gray-500">
-                          체험이 없습니다.
+          <p className="font-32px-bold">예약 현황</p>
+          {myActivities ? (
+            <div className="mt-32">
+              <div className="relative mb-24 lg:mb-30">
+                <p className="font-14px-regular absolute left-12 top-[-10px] z-10 bg-gray-50 px-4">
+                  체험명
+                </p>
+                <Dropdown className="bg-white">
+                  <Dropdown.Trigger
+                    onClick={() => {
+                      setIsOpen(true);
+                    }}
+                    className={clsx(
+                      "font-16px-regular text-left",
+                      "flex h-56 w-full items-center justify-between",
+                      "rounded-4 border border-gray-500",
+                      "px-16 py-15",
+                    )}>
+                    {activityTitle || "체험을 선택하세요"}
+                    {isOpen ? <IconArrowUp /> : <IconArrowDown />}
+                  </Dropdown.Trigger>
+                  <Dropdown.Menu className="w-full bg-white">
+                    {myActivities && myActivities.length > 0 ? (
+                      myActivities.map((activity) => (
+                        <Dropdown.Item
+                          key={activity.id}
+                          className="font-16px-regular flex justify-start py-15 pl-16 text-black hover:bg-gray-100"
+                          onClick={() => {
+                            setSelectedActivityId(activity.id);
+                            setActivityTitle(activity.title);
+                            setIsOpen(false);
+                          }}>
+                          {activity.title}
                         </Dropdown.Item>
-                      )}
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </div>
-                <div className="mb-20">
-                  {selectedActivityId && (
-                    <ReservationHistoryCalendar
-                      activityId={selectedActivityId}
-                    />
-                  )}
-                </div>
+                      ))
+                    ) : (
+                      <Dropdown.Item className="font-16px-regular flex justify-start py-15 pl-16 text-gray-500">
+                        체험이 없습니다.
+                      </Dropdown.Item>
+                    )}
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
-            ) : (
-              <div
-                className={clsx(
-                  "font-24px-medium mt-50 flex flex-col items-center text-gray-700",
-                  "md:mt-64",
-                  "lg:mt-80",
-                )}>
-                <IconEmpty />
-                <p className="mt-37">아직 등록한 체험이 없어요.</p>
+              <div className="mb-20">
+                {selectedActivityId && (
+                  <ReservationHistoryCalendar activityId={selectedActivityId} />
+                )}
               </div>
-            )}
-          </div>
+            </div>
+          ) : (
+            <div
+              className={clsx(
+                "font-24px-medium mt-50 flex flex-col items-center text-gray-700",
+                "md:mt-64",
+                "lg:mt-80",
+              )}>
+              <IconEmpty />
+              <p className="mt-37">아직 등록한 체험이 없어요.</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
