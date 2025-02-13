ed06a519f08fcd66a19700503d8377e13f73dd47
강효성 <goodkingdawn@naver.com>
2025-02-13 15:02:03 +0900

    [fix/#135]시간드롭다운 외부 클릭시  화살표 방향  수정

--- src/components/ActivityInput/TimeDropdown.tsx
+++ src/components/ActivityInput/TimeDropdown.tsx
@@ -1,23 +1,27 @@
 import IconArrowDown from "@/assets/svgs/ic_arrow_down.svg";
 import IconArrowUp from "@/assets/svgs/ic_arrow_up.svg";
 import Dropdown from "@/src/components/Dropdown";
+import useOutsideClick from "@/src/hooks/useOutsideClick";
 import clsx from "clsx";
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 
 interface TimeDropdownProps {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
-  minTime?: string; 
+  minTime?: string;
+  disabled: boolean;
 }
 
 const TimeDropdown = ({
   value,
   onChange,
-  placeholder = "0:00",
+  placeholder = "00:00",
   minTime,
+  disabled = false,
 }: TimeDropdownProps) => {
   const timeOptions = useMemo(() => {
+    if (disabled) return [];
     const options = Array.from({ length: 24 }, (_, i) => ({
       value: `${i}:00`,
       label: `${i < 10 ? `0${i}` : i}:00`,
@@ -32,39 +36,49 @@ const TimeDropdown = ({
     }
 
     return options;
-  }, [minTime]);
+  }, [minTime, disabled]);
 
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const toggleDropdown = () => {
+    if (disabled) return;
+    setIsOpen(!isOpen);
+  };
 
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  useOutsideClick(dropdownRef, () => setIsOpen(false));
 
   return (
-    <Dropdown className="w-79 md:w-104">
-      <Dropdown.Trigger
-        onClick={toggleDropdown}
-        className={clsx(
-          "h-44 py-10 pl-12",
-          "md:h-56 md:px-16 md:py-15",
-          "rounded-4 border border-gray-500",
-          "flex justify-between",
-        )}>
-        {value || placeholder}
-        {isOpen ? <IconArrowUp /> : <IconArrowDown />}
-      </Dropdown.Trigger>
-      <Dropdown.Menu className="max-h-164 overflow-y-auto z-20">
-        {timeOptions.map((option) => (
-          <Dropdown.Item
-            key={option.value}
-            onClick={() => {
-              onChange(option.value);
-              setIsOpen(false);
-            }}
-            className="px-16 py-15 hover:bg-gray-100">
-            {option.label}
-          </Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
+    <div ref={dropdownRef}>
+      <Dropdown className="w-79 bg-white md:w-104">
+        <Dropdown.Trigger
+          onClick={toggleDropdown}
+          className={clsx(
+            "h-44 py-10 pl-12",
+            "md:h-56 md:px-16 md:py-15",
+            "rounded-4 border border-gray-500",
+            "flex justify-between",
+            disabled && "cursor-not-allowed opacity-50",
+          )}>
+          {value || placeholder}
+          {isOpen ? <IconArrowUp /> : <IconArrowDown />}
+        </Dropdown.Trigger>
+        {!disabled && isOpen && (
+          <Dropdown.Menu className="z-20 max-h-164 overflow-y-auto">
+            {timeOptions.map((option) => (
+              <Dropdown.Item
+                key={option.value}
+                onClick={() => {
+                  onChange(option.value);
+                  setIsOpen(false);
+                }}
+                className="px-16 py-15 hover:bg-gray-100">
+                {option.label}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        )}
+      </Dropdown>
+    </div>
   );
 };
 
