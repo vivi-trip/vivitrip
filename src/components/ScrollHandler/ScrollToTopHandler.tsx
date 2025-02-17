836e2aef24b368cd4f95492e3af79041c8e12bb0
JuhyeokC <bos2ablues@gmail.com>
2025-02-17 16:09:06 +0900

    [feat/#159] 스크롤 위로 컴포넌트 동작 수정

--- src/components/ScrollHandler/ScrollToTopHandler.tsx
+++ src/components/ScrollHandler/ScrollToTopHandler.tsx
@@ -1,12 +1,19 @@
 import { useScroll } from "@/src/contexts/ScrollContext";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const ScrollToTopHandler = () => {
+  const router = useRouter();
   const { handleScrollToTop } = useScroll();
 
   useEffect(() => {
-    handleScrollToTop();
-  }, [handleScrollToTop]);
+    const handleRouteChange = () => handleScrollToTop();
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router, handleScrollToTop]);
 
   return null; // UI 요소가 필요 없으므로 아무것도 렌더링하지 않음
 };
