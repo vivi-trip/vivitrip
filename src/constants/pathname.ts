b126570f33879c95e462d3ef6a5473515c13040d
JuhyeokC <bos2ablues@gmail.com>
2025-02-17 20:31:11 +0900

    [feat/#159] 내 체험 관리 경로 생성

--- src/constants/pathname.ts
+++ src/constants/pathname.ts
@@ -13,6 +13,8 @@ const PATH_NAMES: Record<PathNameType | TestPagePathNameType, string> = {
   KakaoSignIn: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}/sign-in`,
   KakaoSignUp: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}/sign-up`,
   Activity: `/activity`,
+  MyActivities: `/my-activities`,
+  MyActivitiesRegistration: `/my-activities/registration`,
 };
 
 export default PATH_NAMES;
