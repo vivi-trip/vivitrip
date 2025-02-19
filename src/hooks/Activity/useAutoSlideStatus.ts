e1b03621e73103d7d1702abd0057175979a1da30
hayuri1990 <hayuri1990@gmail.com>
2025-02-19 16:05:51 +0900

    [feat/#143] sm, md 사이즈에서 hover 시 인기 체험 이동 버튼 표시 및 이동 기능 구현

--- src/hooks/Activity/useAutoSlideStatus.ts
+++ src/hooks/Activity/useAutoSlideStatus.ts
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const useAutoSlideStatus = () => {
   const [isAutoSlide, setIsAutoSlide] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     const updateAutoSlide = () => {
@@ -16,7 +17,13 @@ const useAutoSlideStatus = () => {
     };
   }, []);
 
-  return isAutoSlide;
+  // 자동 슬라이드 중지 & 일정 시간 후 재시작
+  const pauseAutoSlide = useCallback(() => {
+    setIsPaused(true);
+    setTimeout(() => setIsPaused(false), 1000);
+  }, []);
+
+  return { isAutoSlide: isAutoSlide && !isPaused, pauseAutoSlide };
 };
 
 export default useAutoSlideStatus;
