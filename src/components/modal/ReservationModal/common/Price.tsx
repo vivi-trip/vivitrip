19074af45a536dead724d940c6f730fd8e361340
강효성 <goodkingdawn@naver.com>
2025-02-25 14:25:33 +0900

    [fix/#165] 가격 여백 추가 수정

--- src/components/modal/ReservationModal/common/Price.tsx
+++ src/components/modal/ReservationModal/common/Price.tsx
@@ -2,7 +2,7 @@ import formatWage from "@/src/utils/wageFormatter";
 import React from "react";
 
 const Price = ({ price }: { price: number }) => (
-  <div className="flex flex-row items-center gap-5">
+  <div className="flex flex-row items-center gap-5 pt-30 pb-10">
     <p className="font-28px-bold text-black">{formatWage(price)}</p>
     <p className="font-20px-regular text-gray-600">/ 인</p>
   </div>
