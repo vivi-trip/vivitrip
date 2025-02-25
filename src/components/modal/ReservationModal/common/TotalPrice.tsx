c3fa8ea68b2fa5fc0bd3b12649da541b000c0dbb
강효성 <goodkingdawn@naver.com>
2025-02-25 14:28:54 +0900

    [fix/#165] 여백 추가

--- src/components/modal/ReservationModal/common/TotalPrice.tsx
+++ src/components/modal/ReservationModal/common/TotalPrice.tsx
@@ -2,7 +2,7 @@ import formatWage from "@/src/utils/wageFormatter";
 import React from "react";
 
 const TotalPrice = ({ total }: { total: number }) => (
-  <div className="flex justify-between border-t-2 mt-24 pt-16 font-20px-bold">
+  <div className="flex justify-between border-t-2 mt-24 pt-16 font-20px-bold pb-30">
     <p>총 합계</p>
     <div>{formatWage(total)}</div>
   </div>
