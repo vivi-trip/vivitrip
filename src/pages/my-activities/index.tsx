9d42fe43345c2690ca41430d3d2fbf2bd8260363
강효성 <goodkingdawn@naver.com>
2025-02-26 16:55:24 +0900

    [style/#148]불필요 div 삭제

--- src/pages/my-activities/index.tsx
+++ src/pages/my-activities/index.tsx
@@ -11,30 +11,25 @@ const MyActivities = () => {
     router.push(PATH_NAMES.MyActivitiesRegistration);
   };
 
-  
   return (
-    <div>
-      <div className="flex w-full justify-center gap-24 pt-72">
-        <SideNavigationMenu />
-        <div className="flex-1">
-          <div className="flex justify-between">
-            <p className="font-32px-bold">내 체험 관리</p>
-            <Button
-              type="button"
-              width="120"
-              height="48"
-              radius="4"
-              gap="4"
-              fontStyle="l"
-              backgroundColor="black"
-              onClick={handleClick}>
-              체험 등록하기
-            </Button>
-          </div>
-          <div className="">
-            <ActivityList />
-          </div>
+    <div className="flex w-full justify-center gap-24 pt-72">
+      <SideNavigationMenu />
+      <div className="flex-1">
+        <div className="flex justify-between">
+          <p className="font-32px-bold">내 체험 관리</p>
+          <Button
+            type="button"
+            width="120"
+            height="48"
+            radius="4"
+            gap="4"
+            fontStyle="l"
+            backgroundColor="black"
+            onClick={handleClick}>
+            체험 등록하기
+          </Button>
         </div>
+          <ActivityList />
       </div>
     </div>
   );
