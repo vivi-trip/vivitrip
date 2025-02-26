e16a2b3bf5d6721752e941264ed66e0043b7aa77
JuhyeokC <bos2ablues@gmail.com>
2025-02-26 18:09:30 +0900

    [feat/#8] 폼 유효성 검사 규칙 수정 - nickname 에 maxLength 규칙 추가 - 각 규칙 메세지 내용 수정

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -10,6 +10,10 @@ interface ValidationRule {
     value: number;
     message: string;
   };
+  maxLength?: {
+    value: number;
+    message: string;
+  };
   validate?: (value: string) => boolean | string;
 }
 
@@ -20,19 +24,18 @@ const validationRules = (
     required: "이메일을 입력해주세요.",
     pattern: {
       value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-      message: "잘못된 이메일입니다.",
+      message: "이메일 형식으로 입력해 주세요.",
     },
   },
   password: {
     required: "비밀번호를 입력해주세요.",
     minLength: {
       value: 8,
-      message: "비밀번호는 최소 8자 이상이어야 합니다.",
+      message: "8자 이상 입력해주세요.",
     },
     pattern: {
       value: /^(?=.*[A-Z])(?=.*[!@#$%^&*])/,
-      message:
-        "비밀번호에 최소 하나의 대문자와 하나의 특수문자가 포함되어야 합니다.",
+      message: "대문자, 특수 문자를 반드시 포함해야 합니다.",
     },
   },
   confirmPassword: {
@@ -44,7 +47,11 @@ const validationRules = (
     required: "닉네임을 입력해주세요.",
     minLength: {
       value: 2,
-      message: "닉네임은 최소 2자 이상이어야 합니다.",
+      message: "2자 이상 입력해주세요.",
+    },
+    maxLength: {
+      value: 10,
+      message: "10자 이하로 입력해주세요.",
     },
   },
   calendar: {},
