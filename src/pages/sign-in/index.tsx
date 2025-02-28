8cba1ed498674b694c9d7ced72df576f94e18d4b
JuhyeokC <bos2ablues@gmail.com>
2025-02-28 16:43:12 +0900

    [fix/#174] 로그인 페이지 폼 컴포넌트 적용

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,5 +1,5 @@
-import Button from "@/src/components/Button/Button";
 import Dropdown from "@/src/components/Dropdown";
+import Form from "@/src/components/Form";
 import Logo from "@/src/components/Logo";
 import OauthSign from "@/src/components/OauthSign";
 import PATH_NAMES from "@/src/constants/pathname";
@@ -9,20 +9,15 @@ import useUserStore from "@/src/stores/userStore";
 import { SignInProps } from "@/src/types/user";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { FormEvent, useEffect } from "react";
+import { useEffect } from "react";
 
 const SignInRoute = () => {
   const router = useRouter();
   const { userData } = useUserStore();
   const { mutate: signinFn, isPending: isPendingSignin } = useSignIn();
 
-  const handleSignIn = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    const param = Object.fromEntries(formData) as unknown as SignInProps;
-
-    signinFn(param);
+  const handleSignIn = async (data: SignInProps) => {
+    signinFn(data);
   };
 
   useEffect(() => {
@@ -30,50 +25,27 @@ const SignInRoute = () => {
   }, [userData, router]);
 
   return (
-    <div className="mx-auto flex min-h-main w-full max-w-640 flex-col items-stretch justify-center py-48">
-      <Logo size="lg" />
+    <div className="mx-auto min-h-main w-full max-w-640 py-48">
+      <Form onSubmit={handleSignIn} className="gap-16">
+        <Form.Title className="flex-col gap-16">
+          <Logo size="lg" />
+          <p>로그인</p>
+        </Form.Title>
 
-      <form className="mt-56 flex flex-col gap-32" onSubmit={handleSignIn}>
-        <div className="relative">
-          <label htmlFor="login_id" className="flex flex-col gap-8">
-            <p className="font-16px-regular text-basic-black">이메일</p>
-            <input
-              type="email"
-              name="email"
-              id="login_email"
-              placeholder="이메일을 입력해 주세요"
-              className="font-16px-regular min-h-56 rounded-6 border border-gray-500 px-20 py-12 outline-none transition-all focus:border-brand-400"
-            />
-          </label>
-        </div>
+        <Form.Field variant="email">
+          <Form.Input label="이메일" />
+        </Form.Field>
 
-        <div className="relative">
-          <label htmlFor="login_pw" className="flex flex-col gap-8">
-            <p className="font-16px-regular text-basic-black">비밀번호</p>
-            <input
-              type="password"
-              name="password"
-              id="login_password"
-              placeholder="비밀번호을 입력해 주세요"
-              className="font-16px-regular min-h-56 rounded-6 border border-gray-500 px-20 py-12 outline-none transition-all focus:border-brand-400"
-            />
-          </label>
-        </div>
+        <Form.Field variant="password">
+          <Form.Input label="비밀번호" />
+        </Form.Field>
 
-        <div className="relative">
-          <Button
-            type="submit"
-            height="56"
-            fullWidth
-            radius="6"
-            backgroundColor="black"
-            fontStyle="xl"
-            disabled={isPendingSignin}
-            className="disabled:bg-gray-500">
-            로그인 하기
-          </Button>
-        </div>
-      </form>
+        <Form.Field variant="authPage" className="mt-20">
+          <Form.SubmitButton disabled={isPendingSignin}>
+            로그인
+          </Form.SubmitButton>
+        </Form.Field>
+      </Form>
 
       <p className="mt-32 text-center">
         <span className="font-16px-regular text-gray-800">
