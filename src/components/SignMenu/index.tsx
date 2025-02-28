5a7d6ba523b91cb6f8f9f7550da479423d13bb37
JuhyeokC <bos2ablues@gmail.com>
2025-02-28 16:59:17 +0900

    [fix/#174] 반응형 헤더 로그인, 회원가입 버튼 적용

--- src/components/SignMenu/index.tsx
+++ src/components/SignMenu/index.tsx
@@ -1,11 +1,39 @@
+import IcHamburger from "@/assets/svgs/ic_hamburger.svg";
+import Dropdown from "@/src/components/Dropdown";
 import PATH_NAMES from "@/src/constants/pathname";
+import useWindowSize from "@/src/hooks/useWindowSize";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const CLASS_NAME = {
   text: "font-16px-medium text-basic-black text-nowrap",
 };
 
 const SignMenu = () => {
+  const router = useRouter();
+  const windowSize = useWindowSize();
+
+  if (windowSize === "sm")
+    return (
+      <Dropdown>
+        <Dropdown.Trigger>
+          <IcHamburger width={32} height={32} />
+        </Dropdown.Trigger>
+        <Dropdown.Menu className="left-auto right-0 !border-gray-200 !bg-gray-200 shadow-lg">
+          <Dropdown.Item
+            className="min-h-48 min-w-120 bg-white"
+            onClick={() => router.push(PATH_NAMES.SignIn)}>
+            로그인
+          </Dropdown.Item>
+          <Dropdown.Item
+            className="mt-1 min-h-48 min-w-120 bg-white"
+            onClick={() => router.push(PATH_NAMES.SignUp)}>
+            회원가입
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+
   return (
     <div className="flex items-center gap-24">
       <Link href={PATH_NAMES.SignIn} className={CLASS_NAME.text}>
