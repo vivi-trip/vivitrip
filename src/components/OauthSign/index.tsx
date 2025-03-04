c40415f7fac71d3baf7d02c54d66c8d554c6a1f2
JuhyeokC <bos2ablues@gmail.com>
2025-03-04 18:46:58 +0900

    [fix/#174] SNS 계정 연동 반응형 UI 적용

--- src/components/OauthSign/index.tsx
+++ src/components/OauthSign/index.tsx
@@ -1,13 +1,21 @@
 import OauthSignButton from "@/src/components/OauthSignButton";
+import useWindowSize from "@/src/hooks/useWindowSize";
 import { OauthSignProps } from "@/src/types/oauth";
+import clsx from "clsx";
 
 const OauthSign = ({ action }: OauthSignProps) => {
+  const windowSize = useWindowSize();
+
   return (
     <div className="mt-48">
-      <div className="flex items-center gap-36">
+      <div className="flex items-center justify-center gap-[4%]">
         <i className="h-1 w-full bg-gray-200" />
-        <p className="font-20px-regular text-nowrap text-gray-700">
-          SNS 계정으로 {action === "in" ? "로그인" : "회원가입"}하기
+        <p
+          className={clsx(
+            "font-20px-regular w-full flex-1 text-nowrap text-center text-gray-700",
+            windowSize === "xs" && "!whitespace-normal break-keep",
+          )}>
+          {`SNS 계정으로 ${action === "in" ? "로그인" : "회원가입"}하기`}
         </p>
         <i className="h-1 w-full bg-gray-200" />
       </div>
