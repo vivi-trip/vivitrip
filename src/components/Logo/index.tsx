1f649feba0c8d917fdf4dfacc988d82843844518
JuhyeokC <bos2ablues@gmail.com>
2025-03-04 18:41:51 +0900

    [fix/#174] 로고 sm 사이즈 추가

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,18 @@
 import VIVITRIP_LOGO_HORIZONTAL from "@/assets/logo/vivitrip_logo_horizontal.svg";
+import VIVITRIP_LOGO_ICON from "@/assets/logo/vivitrip_logo_icon.svg";
 import VIVITRIP_LOGO_VERTICAL from "@/assets/logo/vivitrip_logo_vertical.svg";
 import PATH_NAMES from "@/src/constants/pathname";
 import Link from "next/link";
 import { ReactNode } from "react";
 
-type LogoSizeType = "md" | "lg";
+type LogoSizeType = "sm" | "md" | "lg";
 
 interface LogoProps {
   size?: LogoSizeType;
 }
 
 const LOGO_SIZE: Record<LogoSizeType, ReactNode> = {
+  sm: <VIVITRIP_LOGO_ICON className="h-28" />,
   md: <VIVITRIP_LOGO_HORIZONTAL className="h-28" />,
   lg: <VIVITRIP_LOGO_VERTICAL className="h-192" />,
 };
