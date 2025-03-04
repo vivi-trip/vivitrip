d0618a6bfc9c723a0b90ef2fb156a2ef9c9d3590
hayuri1990 <hayuri1990@gmail.com>
2025-03-05 06:38:43 +0900

    [refactor/#180] Carousel 버튼 클래스 공통화하여 중복 제거

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -55,6 +55,9 @@ const Carousel = ({ items }: CarouselProps) => {
     };
   }, [currentIndex, extendedItems.length]);
 
+  const commonButtonClass =
+    "font-16px-regular lg:font-24px-regular absolute top-1/2 h-50 w-25 -translate-y-1/2 rounded-full bg-basic-navy text-white hover:bg-brand-600 lg:h-60 lg:w-30";
+
   return (
     <div className="relative h-240 w-full overflow-hidden md:h-550">
       <div
@@ -100,13 +103,13 @@ const Carousel = ({ items }: CarouselProps) => {
       <Button
         type="button"
         onClick={() => goToPrevious()}
-        className="font-16px-regular lg:font-24px-regular absolute left-16 top-1/2 h-50 w-25 -translate-y-1/2 rounded-full bg-basic-navy text-white hover:bg-brand-600 md:left-24 lg:h-60 lg:w-30">
+        className={`left-16 md:left-24 ${commonButtonClass}`}>
         &#8249;
       </Button>
       <Button
         type="button"
         onClick={() => goToNext()}
-        className="font-16px-regular lg:font-24px-regular absolute right-16 top-1/2 h-50 w-25 -translate-y-1/2 rounded-full bg-basic-navy text-white hover:bg-brand-600 md:right-24 lg:h-60 lg:w-30">
+        className={`right-16 md:right-24 ${commonButtonClass}`}>
         &#8250;
       </Button>
     </div>
