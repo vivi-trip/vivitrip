7bc2274c6d8e6940d43bc1d9b8fbb9aff77581c1
JuhyeokC <bos2ablues@gmail.com>
2025-03-06 17:27:57 +0900

    [fix/#177] 토큰 관리 제거

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,48 +1,31 @@
 import { OauthTypes } from "@/src/types/oauth";
 import { User } from "@/src/types/user";
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
-
-export async function getServerSideProps() {
-  const user = { name: null };
-  return {
-    props: { user }, // 페이지에 전달
-  };
-}
+import { persist } from "zustand/middleware";
 
 interface UserStore {
   userData: User | null;
   userProvider: OauthTypes | null;
-  accessToken: string | null;
-  refreshToken: string | null;
   setUserData: (data: User) => void;
   setUserProvider: (provider: OauthTypes) => void;
-  setTokens: (accessToken: string, refreshToken: string) => void;
   clearUser: () => void;
 }
 
-const useUserStore = create<UserStore>()(
-  persist(
+const useUserStore = create(
+  persist<UserStore>(
     (set) => ({
       userData: null,
       userProvider: null,
-      accessToken: null,
-      refreshToken: null,
       setUserData: (data) => set({ userData: data }),
       setUserProvider: (provider) => set({ userProvider: provider }),
-      setTokens: (accessToken, refreshToken) =>
-        set({ accessToken, refreshToken }),
       clearUser: () =>
         set({
           userData: null,
           userProvider: null,
-          accessToken: null,
-          refreshToken: null,
         }),
     }),
     {
-      name: "vivitrip-user-storage", // 저장소의 항목 이름(고유해야 함)
-      storage: createJSONStorage(() => sessionStorage), // (선택 사항) 기본적으로 'localStorage'가 사용됩니다.
+      name: String(process.env.NEXT_PUBLIC_USER_STORAGE_NAME), // 저장소의 항목 이름(고유해야 함)
     },
   ),
 );
