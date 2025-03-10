21d33fcf158a47081b274064e250ebb3c2e57bf8
강효성 <goodkingdawn@naver.com>
2025-03-10 15:15:38 +0900

    [fix/#183]예약 완료 ->  예약 신청 문구 수정

--- src/constants/reserbationStatus.ts
+++ src/constants/reserbationStatus.ts
@@ -1,4 +1,4 @@
-import { ReservationStatus } from "../types/my-reservations";
+import { ReservationStatus } from "@/src/types/my-reservations";
 
 
 type ReservationLabels =
@@ -6,7 +6,7 @@ type ReservationLabels =
   | "예약 취소"
   | "예약 거절"
   | "예약 승인"
-  | "예약 완료";
+  | "예약 신청";
 
   const RESERVATION_LABEL: Record<
   ReservationStatus,
@@ -16,9 +16,8 @@ type ReservationLabels =
   canceled: { label: "예약 취소", colorClass: "text-gray-500" },
   declined: { label: "예약 거절", colorClass: "text-red-200" },
   confirmed: { label: "예약 승인", colorClass: "text-orange-100" },
-  pending: { label: "예약 완료", colorClass: "text-blue-100" },
-  all: {}, // `label`과 `colorClass`는 빈 객체로 두어서 사용하지 않음
-  "": {}, // `label`과 `colorClass`는 빈 객체로 두어서 사용하지 않음
+  pending: { label: "예약 신청", colorClass: "text-blue-100" },
+  "": {}, 
 };
 
 export default RESERVATION_LABEL;
