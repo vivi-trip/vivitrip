2e811fd61f4f97ad6aa9512234bdaf6604351b7e
JuhyeokC <bos2ablues@gmail.com>
2025-03-12 17:12:16 +0900

    [feat/#144] 내 체험 관리 수정 - 태그 수정 button -> Link - 불필요한 div 태그 제거 - style 프롭 테일윈드로 수정 - 클래스 정리

--- src/components/MyAtivities/ActivityCard.tsx
+++ src/components/MyAtivities/ActivityCard.tsx
@@ -4,7 +4,7 @@ import PATH_NAMES from "@/src/constants/pathname";
 import { Activity } from "@/src/types/activitiesReservationType";
 import clsx from "clsx";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 interface ActivitiesCardProps {
   activity: Activity;
@@ -20,75 +20,53 @@ const ActivitiesCard = ({ activity }: ActivitiesCardProps) => {
     price,
   } = activity;
 
-  const router = useRouter();
-
   return (
-    <div
+    <Link
+      href={`${PATH_NAMES.Activity}/${activityId}`}
       className={clsx(
-        "relative mt-24 h-128 w-full min-w-280",
+        "relative flex",
+        "rounded-24 border border-white shadow-[0px_4px_16px_0px_rgba(0,0,0,0.08)]",
+        "mt-24 h-128 w-full min-w-280",
         "md:h-156",
         "lg:h-204",
       )}>
-      <button
-        type="button"
-        onClick={() => {
-          router.push(`${PATH_NAMES.Activity}/${activityId}`);
-        }}
+      <div
         className={clsx(
-          "h-128 w-full rounded-24 border border-white",
-          "md:h-156",
-          "lg:h-204",
-        )}
-        style={{ boxShadow: "0px 4px 16px 0px rgba(0, 0, 0, 0.08)" }}>
-        <div className="flex">
-          <div
-            className={clsx(
-              "relative",
-              "h-128 min-w-128",
-              "md:h-156 md:min-w-156",
-              "lg:h-204 lg:min-w-204",
-            )}>
-            <Image
-              src={bannerImageUrl}
-              alt="체험 이미지"
-              fill
-              style={{ objectFit: "cover" }}
-              className="rounded-l-24 border border-brand-200 bg-brand-400"
-            />
-          </div>
-          <div
-            className={clsx(
-              "my-9 ml-8 mr-14 flex w-full flex-col",
-              "md:m-12 md:mr-18 md:gap-6 lg:my-14 lg:ml-24",
-            )}>
-            <div className="flex items-center gap-6">
-              <IconStar />
-              <span className="font-14px-regular md:font-16px-regular">
-                {rating} ({reviewCount})
-              </span>
-            </div>
-            <div className="flex h-full flex-col items-start justify-between">
-              <p  className={clsx(
-              "font-14px-bold text-left",
-              "w-full max-w-200 truncate",
-              "md:font-18px-bold md:max-w-300",
-              "lg:font-20px-bold lg:mt-8 lg:max-w-480",
-            )}>
-                {title}
-              </p>
-              <div className="flex w-full items-center justify-between">
-                <p className="font-16px-medium md:font-20px-medium lg:font-24px-medium lg:text-gray-700">
-                  ₩{price?.toLocaleString("ko-KR")} /인
-                </p>
-                <div>
-                  <MyActivityHandler activityId={activityId} />
-                </div>
-              </div>
-            </div>
+          "relative overflow-hidden",
+          "rounded-l-24",
+          "h-full min-w-128",
+          "md:min-w-156",
+          "lg:min-w-204",
+        )}>
+        <Image
+          src={bannerImageUrl}
+          alt="체험 이미지"
+          fill
+          className="border border-brand-200 bg-brand-400 object-cover"
+        />
+      </div>
+      <div
+        className={clsx(
+          "flex w-full flex-col",
+          "p-12 px-16",
+          "md:p-16 md:px-20",
+          "lg:p-20 lg:px-24",
+        )}>
+        <div className="flex items-center gap-6">
+          <IconStar />
+          <span className="font-14px-regular md:font-16px-regular">{`${rating} (${reviewCount})`}</span>
+        </div>
+        <div className="flex h-full flex-col items-start justify-between">
+          <p className="font-14px-bold md:font-20px-bold">{title}</p>
+          <div className="flex w-full items-center justify-between">
+            <p className="font-16px-medium md:font-20px-medium lg:font-24px-medium lg:text-gray-700">
+              {`₩${price?.toLocaleString("ko-KR")} / 인`}
+            </p>
+            <MyActivityHandler activityId={activityId} />
           </div>
         </div>
-      </button>
-    </div>
+      </div>
+    </Link>
   );
 };
 
