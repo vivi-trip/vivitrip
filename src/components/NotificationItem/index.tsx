1c9970ef8ab75cf32eec344df2ebf4a8201d4410
JuhyeokC <bos2ablues@gmail.com>
2025-03-12 17:04:50 +0900

    [feat/#144] 알림 내용 클릭 시 예약 내역으로 이동

--- src/components/NotificationItem/index.tsx
+++ src/components/NotificationItem/index.tsx
@@ -2,6 +2,7 @@
 import IconClose from "@/assets/svgs/close.svg";
 import HighlightText from "@/src/components/HighlightText/HighlightText";
 import STATUS_KEYWORD_HIGHLIGHT from "@/src/constants/my-notifications";
+import PATH_NAMES from "@/src/constants/pathname";
 import type { MyNotificationsProps } from "@/src/types/my-notifications";
 import { getTimeDiffText } from "@/src/utils/date";
 import clsx from "clsx";
@@ -15,16 +16,6 @@ const NotificationItem = ({
   item: MyNotificationsProps;
   handleDelete: () => void;
 }) => {
-  /**
-   * @todo
-   * 1.
-   * swagger 에서 응답 객체에는 { id, teamId, userId, content, createdAt, updatedAt, deletedAt } 7가지가 전부이다.
-   * 예약 상태에는 [ pending, confirmed, declined, canceled, completed ] 5가지가 있다.
-   * 실제 데이터를 확인하여 예약상태를 구현할 것.
-   *
-   * 2.
-   * 알람을 클릭하면 마이페이지의 예약 내역(승인|거절), 예약 현황(새로 들어왔어요.) 으로 이동하는 링크 구현
-   */
   const statusColor = useMemo<string | null>(() => {
     if (item.content.includes("승인")) {
       return `bg-${STATUS_KEYWORD_HIGHLIGHT["승인"]}`;
@@ -41,15 +32,6 @@ const NotificationItem = ({
     return null;
   }, [item]);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const MOCK_PATHNAME = {
-    blue: `/my-page`,
-    red: `/my-page`,
-    green: `/my-page`,
-  };
-
-  console.log(statusColor?.split("-")[1]);
-
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     handleDelete();
@@ -57,7 +39,7 @@ const NotificationItem = ({
 
   return (
     <Link
-      href="mypage"
+      href={PATH_NAMES.MyReservations}
       className="flex flex-col items-stretch justify-center gap-4 rounded border border-brand-100 bg-white p-12 shadow-md">
       <div className="flex items-center justify-between">
         <p className={clsx(statusColor, "size-5 rounded-full")} />
