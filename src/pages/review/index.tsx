a3754485683f6618109cff8ef3ffd2ba655e21eb
hayuri1990 <hayuri1990@gmail.com>
2025-03-16 18:09:31 +0900

    [fix/#168] build 에러 해결

--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,46 +1,26 @@
 import Review from "@/src/components/Review";
-import { getActivityReviews } from "@/src/services/activities";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { listAllActivities } from "@/src/services/activities";
+import { InferGetServerSidePropsType } from "next";
 
-export const getServerSideProps = async (
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  context: GetServerSidePropsContext,
-) => {
-  // const {activityId} = context.query;
-  const activityId = 3613; // 임시
-  const reviewInitialData = await getActivityReviews({ activityId });
-  const reviewTotalCount = reviewInitialData.totalCount;
+export const getServerSideProps = async () => {
+  const response = await listAllActivities("latest");
+  const { activities } = response;
 
-  const reviewData = await getActivityReviews({
-    activityId,
-    page: 1,
-    size: reviewTotalCount,
-  });
-
-  const { reviews, totalCount, averageRating } = reviewData;
+  // activityId 랜덤으로 선택
+  const randomIndex = Math.floor(Math.random() * activities.length);
+  const activityId = activities[randomIndex].id;
 
   return {
     props: {
       activityId,
-      reviews,
-      totalCount,
-      averageRating,
     },
   };
 };
 
 const ReviewPage = ({
-  reviews,
-  totalCount,
-  averageRating,
+  activityId,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return (
-    <Review
-      reviews={reviews}
-      totalCount={totalCount}
-      averageRating={averageRating}
-    />
-  );
+  return <Review activityId={activityId} />;
 };
 
 export default ReviewPage;
