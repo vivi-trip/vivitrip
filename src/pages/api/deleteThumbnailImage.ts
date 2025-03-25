6d253bec4d6a3c87d5fe5412a35d68eb9258ba5b
JuhyeokC <bos2ablues@gmail.com>
2025-03-25 13:02:04 +0900

    [feat/#222] 썸네일 파일 삭제 함수 수정 - 파일 경로 생성 상수 변경 - 저장 경로 내 이미지 확인 위치 수정 - 메세지 수정

--- src/pages/api/deleteThumbnailImage.ts
+++ src/pages/api/deleteThumbnailImage.ts
@@ -4,36 +4,35 @@ import path from "path";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
+    return res.status(405).json({ error: "허용되지 않는 메소드입니다" });
   }
 
   const { id } = req.body;
   if (!id) {
-    return res.status(400).json({ error: "id is required" });
+    return res.status(400).json({ error: "id 는 필수입니다" });
   }
 
   // 이미지 파일명
   const imageFileName = `${id}.jpeg`;
 
-  // 로컬 저장 경로
-  const imagePath = path.join(
-    process.cwd(),
-    "public",
-    "images",
-    "thumbnail",
-    imageFileName,
-  );
+  // 저장 경로
+  const rootDir = path.join(process.cwd(), "public", "images", "thumbnail");
+
+  // 이미지 경로
+  const imagePath = path.resolve(rootDir, imageFileName);
+
+  // 저장 경로 내 이미지 확인
+  if (!imagePath.startsWith(rootDir)) {
+    return res.status(400).json({ error: "파일이 존재하지 않습니다" });
+  }
 
   try {
-    // 이미지 존재 확인
-    if (fs.existsSync(imagePath)) {
-      // 이미지 삭제
-      await fs.promises.unlink(imagePath);
-      return res.status(200).json({ message: "이미지 삭제 완료" });
-    }
-    return res.status(500).json({ error: "이미지가 존재하지 않음" });
+    // 이미지 삭제
+    await fs.promises.unlink(imagePath);
+
+    return res.status(200).json({ message: "이미지 파일 삭제 성공" });
   } catch (error) {
-    return res.status(500).json({ error: "이미지 삭제 실패" });
+    return res.status(500).json({ error: "이미지 파일 삭제 실패" });
   }
 };
 
