dcea243a0ef40c4bfffd6044f7eac97a24d8badc
JuhyeokC <bos2ablues@gmail.com>
2025-03-25 13:55:33 +0900

    [feat/#222] 썸네일 이미지 다운로드 함수 오류 처리 방식 수정

--- src/utils/downloadThumbnailImage.ts
+++ src/utils/downloadThumbnailImage.ts
@@ -20,7 +20,7 @@ const downloadThumbnailImage = async ({
   // 저장 경로를 조회하고 없으면 생성
   const dir = path.dirname(savePath);
   if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
+    fs.promises.mkdir(dir, { recursive: true });
   }
 
   // 저장 경로에 이미지가 존재하면 다운로드 하지 않음
@@ -28,25 +28,25 @@ const downloadThumbnailImage = async ({
     return;
   }
 
-  // 서버에 이미지 다운로드
-  const res = await fetch(imageUrl);
+  try {
+    // 서버에 이미지 다운로드
+    const res = await fetch(imageUrl);
 
-  if (!res.ok) {
+    // 파일데이터 버퍼 변환
+    const arrayBuffer = await res.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    // 이미지 최적화
+    const optimizedImage = await sharp(buffer)
+      .resize(400, 210, { fit: "cover" })
+      .jpeg({ quality: 8 })
+      .toBuffer();
+
+    // 이미지 데이터를 로컬 파일 시스템에 저장
+    await fs.promises.writeFile(savePath, optimizedImage);
+  } catch (error) {
     throw new Error("이미지 다운로드 실패");
   }
-
-  // 파일데이터 버퍼 변환
-  const arrayBuffer = await res.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-
-  // 이미지 최적화
-  const optimizedImage = await sharp(buffer)
-    .resize(400, 210, { fit: "cover" })
-    .jpeg({ quality: 8 })
-    .toBuffer();
-
-  // 이미지 데이터를 로컬 파일 시스템에 저장
-  await fs.promises.writeFile(savePath, optimizedImage);
 };
 
 export default downloadThumbnailImage;
