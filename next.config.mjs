d420ca24771a7a57dad4ad8e212eb305de6da665
JuhyeokC <bos2ablues@gmail.com>
2025-03-25 20:27:23 +0900

    [feat/#222] 썸네일 확인 용 허용 이미지 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "vivitrip-thumbnail.s3.ap-northeast-2.amazonaws.com",
+      },
     ],
   },
 };
