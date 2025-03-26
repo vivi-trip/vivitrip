8f3f5ab1ba8d17721e35f52dcff2ea00982cc710
JuhyeokC <bos2ablues@gmail.com>
2025-03-26 16:43:35 +0900

    [chore/#237] footer 정리 - 테스트페이지 바로가기, Privacy Policy, FAQ 버튼 삭제

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -2,8 +2,6 @@ import IconFacebook from "@/assets/svgs/ic_sns_facebook_fill.svg";
 import IconInstagram from "@/assets/svgs/ic_sns_instagram_fill.svg";
 import IconTwitter from "@/assets/svgs/ic_sns_twitter_fill.svg";
 import IconYoutube from "@/assets/svgs/ic_sns_youtube_fill.svg";
-import TestPages from "@/src/components/TestPages";
-import PATH_NAMES from "@/src/constants/pathname";
 import Link from "next/link";
 
 const SNS_LINKS = [
@@ -35,20 +33,10 @@ const CLASS_NAME = {
 
 const Footer = () => {
   return (
-    <footer className="min-h-footer -mx-24 bg-brand-500 p-32 md:-mx-32">
+    <footer className="-mx-24 min-h-footer bg-brand-500 p-32 md:-mx-32">
       <div className="mx-auto flex h-full max-w-screen-xl flex-wrap items-start justify-between gap-x-12 gap-y-40 md:gap-x-32">
         <div className="flex items-center">
-          <p className={CLASS_NAME.text}>©vivitrip - 2024</p>
-          <hr className="mx-12 h-16 border-0 border-l border-white opacity-50 md:mx-24" />
-          <TestPages />
-        </div>
-        <div className="flex items-center gap-12 md:gap-32">
-          <Link className={CLASS_NAME.text} href={PATH_NAMES.PrivacyPolicy}>
-            Privacy Policy
-          </Link>
-          <Link className={CLASS_NAME.text} href={PATH_NAMES.FAQ}>
-            FAQ
-          </Link>
+          <p className={CLASS_NAME.text}>©VIVITRIP - 2025</p>
         </div>
         <div className="flex items-center gap-12 md:gap-16">
           {SNS_LINKS.map(({ name, icon, href }) => {
