0ee9e01475c37ae96bdd8aa2ce37276a1a90f5d3
JuhyeokC <bos2ablues@gmail.com>
2025-03-26 18:02:46 +0900

    [chore/#241] 테스트 유저 로그인 기능 제거

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,10 +1,8 @@
-import Dropdown from "@/src/components/Dropdown";
 import Form from "@/src/components/Form";
 import Logo from "@/src/components/Logo";
 import OauthSign from "@/src/components/OauthSign";
 import SignOptionSection from "@/src/components/SignOptionSection";
 import PATH_NAMES from "@/src/constants/pathname";
-import TEST_USERS from "@/src/constants/testUsers";
 import SignPageWrap from "@/src/containers/SignPageWrap";
 import { useSignIn } from "@/src/queries/auth";
 import useUserStore from "@/src/stores/useUserStore";
@@ -52,24 +50,6 @@ const SignInRoute = () => {
       <SignOptionSection action="in" />
 
       <OauthSign action="in" />
-
-      <div className="flex justify-end">
-        <Dropdown>
-          <Dropdown.Trigger>테스트 유저 로그인</Dropdown.Trigger>
-          <Dropdown.Menu className="!bottom-full !left-auto !right-0 !top-auto !mb-4 !mt-0 flex flex-col gap-4 p-4">
-            {TEST_USERS.map((item) => {
-              return (
-                <Dropdown.Item
-                  key={`test_user_item_${item.email}`}
-                  className="!rounded border border-brand-300 bg-brand-200 px-24 py-2 text-brand-500"
-                  onClick={() => signinFn(item)}>
-                  {item.nickname}
-                </Dropdown.Item>
-              );
-            })}
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
     </SignPageWrap>
   );
 };
