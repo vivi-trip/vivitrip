155948e9eb4459daadb845c850276405cc8de268
JuhyeokC <bos2ablues@gmail.com>
2025-04-02 19:26:14 +0900

    [feat/#264] 리다이렉트 방식 변경 - 미들웨어에서 체크 - 유저 상태 없을 때 빈화면 출력

--- src/pages/my-activities/index.tsx
+++ src/pages/my-activities/index.tsx
@@ -3,14 +3,20 @@ import ActivityList from "@/src/components/MyActivities/ActivityList";
 import SideNavigationMenu from "@/src/components/SideNavigationMenu/SideNavigationMenu";
 import PATH_NAMES from "@/src/constants/pathname";
 import MyPageWrap from "@/src/containers/MyPageWrap";
+import useUserStore from "@/src/stores/useUserStore";
 import { useRouter } from "next/router";
 
 const MyActivities = () => {
   const router = useRouter();
+  const { userData } = useUserStore.getState();
 
   const handleClick = () => {
     router.push(PATH_NAMES.MyActivitiesRegistration);
   };
+
+  if (!userData) {
+    return null;
+  }
 
   return (
     <MyPageWrap>
